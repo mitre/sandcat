f4932687d4f1d99e13aa5e76bd4f680ce8ad5b8c
david <davidhunt@mitre.org>
2019-05-13 10:47:23 -0400

    updating file stores

--- hook.py
+++ hook.py
@@ -3,7 +3,6 @@ from plugins.sandcat.app.sand_api import SandApi
 name = 'Sandcat'
 description = 'An in-memory agent/server combination'
 address = '/plugin/sandcat/gui'
-store = None
 
 
 async def initialize(app, services):
@@ -13,6 +12,4 @@ async def initialize(app, services):
     services.get('auth_svc').set_unauthorized_route('POST', '/sand/results', cat_api.results)
     services.get('auth_svc').set_unauthorized_route('POST', '/sand/register', cat_api.registration)
     services.get('auth_svc').set_unauthorized_route('POST', '/sand/instructions', cat_api.instructions)
-    services.get('auth_svc').set_unauthorized_route('POST', '/file/render', cat_api.render)
-    services.get('auth_svc').set_unauthorized_route('POST', '/file/download', cat_api.download)
 
