45126e030e8f50df5b48badd6de5caccf3cd05ca
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-06-15 08:02:10 -0400

    reworking the gui (#19)

--- hook.py
+++ hook.py
@@ -2,13 +2,11 @@ from plugins.sandcat.app.sand_api import SandApi
 
 name = 'Sandcat'
 description = 'A custom multi-platform RAT'
-address = '/plugin/sandcat/gui'
+address = None
 
 
 async def initialize(app, services):
     cat_api = SandApi(services=services)
-    app.router.add_static('/sandcat', 'plugins/sandcat/static/', append_version=True)
-    services.get('auth_svc').set_unauthorized_route('GET', '/plugin/sandcat/gui', cat_api.splash)
     services.get('auth_svc').set_unauthorized_route('POST', '/sand/results', cat_api.results)
     services.get('auth_svc').set_unauthorized_route('POST', '/sand/register', cat_api.registration)
     services.get('auth_svc').set_unauthorized_route('POST', '/sand/instructions', cat_api.instructions)
