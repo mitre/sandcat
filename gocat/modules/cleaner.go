e69a13ba07da47872509194282b61cb411506745
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-06-20 12:00:18 -0500

    ensuring cleanup happens on agent-side (#29)

---
+++ gocat/modules/cleaner.go
@@ -0,0 +1,34 @@
+package modules
+
+import (
+	"os"
+	"path/filepath"
+)
+
+var cleanups []string
+var payloads []string
+
+// ApplyCleanup stores cleanup actions
+func ApplyCleanup(command map[string]interface{}) {
+	cleanup := command["cleanup"].(string)
+	payload := command["payload"].(string)
+	if len(cleanup) > 0 {
+		cleanups = append(cleanups, cleanup)
+	}
+	if len(payload) > 0 {
+		payloads = append(payloads, payload)
+	}
+}
+
+// Cleanup runs all cleanup activities
+func Cleanup(files string) {
+	for i := range cleanups {
+		cmd := Decode(cleanups[len(cleanups)-1-i])
+		Execute(string(cmd))
+	}
+	for _, value := range payloads {
+		os.Remove(filepath.Join(files, value))
+	}
+	cleanups = cleanups[:0]
+	payloads = payloads[:0]
+}
