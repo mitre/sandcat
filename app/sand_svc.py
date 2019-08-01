789bdd3defb9a66826f73e8c788ccc9ca30b8022
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-08-01 07:50:02 -0400

    ensuring dao is not used directly (#38)
    Former-commit-id: f7a130921142cc7985e49d178fdd12e3370ca555

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -23,27 +23,27 @@ class SandService:
             os.system('GOOS=%s go build -o %s -ldflags="-s -w" %s' % (platform, output, main_module))
 
     async def beacon(self, paw, platform, server, group, files):
-        agent = await self.data_svc.dao.get('core_agent', dict(paw=paw))
+        agent = await self.data_svc.explode_agents(criteria=dict(paw=paw))
         if agent:
             self.log.debug('Beacon (%s)' % paw)
             last_seen = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             updated = dict(last_seen=last_seen, checks=agent[0]['checks'] + 1, platform=platform, server=server)
-            await self.data_svc.dao.update('core_agent', 'paw', paw, data=updated)
+            await self.data_svc.update('core_agent', 'paw', paw, data=updated)
             await self.data_svc.create_group(name=group, paws=[paw])
             return agent[0]['id']
         else:
             self.log.debug('New beacon (%s)' % paw)
             queued = dict(last_seen=datetime.now(), paw=paw, checks=1, platform=platform, server=server, files=files)
-            agent_id = await self.data_svc.dao.create('core_agent', queued)
+            agent_id = await self.data_svc.create_agent(agent=queued)
             await self.data_svc.create_group(name=group, paws=[paw])
             return agent_id
 
     async def instructions(self, agent_id):
-        sql = 'SELECT * FROM core_chain where host_id = %s and collect is null' % agent_id
+        commands = await self.data_svc.explode_chain(criteria=dict(host_id=agent_id))
         instructions = []
-        for link in await self.data_svc.dao.raw_select(sql):
-            await self.data_svc.dao.update('core_chain', key='id', value=link['id'], data=dict(collect=datetime.now()))
-            payload = await self.data_svc.dao.get('core_payload', dict(ability=link['ability']))
+        for link in [c for c in commands if not c['collect']]:
+            await self.data_svc.update('core_chain', key='id', value=link['id'], data=dict(collect=datetime.now()))
+            payload = await self.data_svc.explode_payloads(criteria=dict(ability=link['ability']))
             instructions.append(json.dumps(dict(id=link['id'], sleep=link['jitter'],
                                                 command=link['command'], cleanup=link['cleanup'],
                                                 payload=payload[0]['payload'] if payload else '')))
@@ -52,8 +52,8 @@ class SandService:
     async def post_results(self, paw, link_id, output, status):
         self.log.debug('[AGENT] posting results (%s)' % paw)
         finished = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        await self.data_svc.dao.create('core_result', dict(link_id=link_id, output=output))
-        await self.data_svc.dao.update('core_chain', key='id', value=link_id, data=dict(status=int(status), finish=finished))
+        await self.data_svc.create_result(result=dict(link_id=link_id, output=output))
+        await self.data_svc.update('core_chain', key='id', value=link_id, data=dict(status=int(status), finish=finished))
         return json.dumps(dict(status=True))
 
     """ PRIVATE """
