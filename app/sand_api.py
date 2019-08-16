7b386402266f40bc42cc98a006b5e776b46f915e
david <davidhunt@mitre.org>
2019-08-16 15:54:28 -0400

    moving generic functions to agent_svc

--- app/sand_api.py
+++ app/sand_api.py
@@ -3,13 +3,10 @@ from datetime import datetime
 
 from aiohttp import web
 
-from plugins.sandcat.app.sand_svc import SandService
-
 
 class SandApi:
 
     def __init__(self, services):
-        self.sand_svc = SandService(services)
         self.utility_svc = services.get('utility_svc')
         self.file_svc = services.get('file_svc')
         self.agent_svc = services.get('agent_svc')
@@ -19,12 +16,11 @@ class SandApi:
         data = json.loads(self.utility_svc.decode_bytes(await request.read()))
         data['server'] = '%s://%s' % (request.scheme, request.host)
         await self.agent_svc.handle_heartbeat(paw, **data)
-        instructions = await self.sand_svc.instructions(paw)
+        instructions = await self.agent_svc.get_instructions(paw)
         return web.Response(text=self.utility_svc.encode_string(instructions))
 
     async def results(self, request):
-        paw = request.headers.get('X-PAW')
         data = json.loads(self.utility_svc.decode_bytes(await request.read()))
         data['time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        status = await self.sand_svc.post_results(paw, data['link_id'], data['output'], data['status'])
+        status = await self.agent_svc.save_results(data['link_id'], data['output'], data['status'])
         return web.Response(text=self.utility_svc.encode_string(status))
