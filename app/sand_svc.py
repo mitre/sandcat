3d36ba40e49ab35f1b15b60af6b1c850fa9ea3f6
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-08-16 14:40:48 -0400

    ensuring agents can be managed modular (#41)
    * ensuring agents can be managed modular
    
    * ensuring agents can be managed modular
    
    
    Former-commit-id: b35764edd413e708d79fb30bdfd31e6f7fe3498a

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -7,37 +7,34 @@ class SandService:
 
     def __init__(self, services):
         self.data_svc = services.get('data_svc')
+        self.file_svc = services.get('file_svc')
         self.utility_svc = services.get('utility_svc')
         self.log = self.utility_svc.create_logger('sandcat')
 
-    async def beacon(self, paw, platform, server, group, files):
-        agent = await self.data_svc.explode_agents(criteria=dict(paw=paw))
-        if agent:
-            self.log.debug('Beacon (%s)' % paw)
-            last_seen = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            updated = dict(last_seen=last_seen, checks=agent[0]['checks'] + 1, platform=platform, server=server)
-            await self.data_svc.update('core_agent', 'paw', paw, data=updated)
-            return agent[0]['id']
-        else:
-            self.log.debug('New beacon (%s)' % paw)
-            queued = dict(last_seen=datetime.now(), paw=paw, checks=1, platform=platform, server=server, files=files, host_group=group)
-            agent_id = await self.data_svc.create_agent(agent=queued)
-            return agent_id
-
-    async def instructions(self, agent_id):
-        commands = await self.data_svc.explode_chain(criteria=dict(host_id=agent_id))
+    async def instructions(self, paw):
+        self.log.debug('%s checking for instructions' % paw)
+        commands = await self.data_svc.explode_chain(criteria=dict(paw=paw))
         instructions = []
         for link in [c for c in commands if not c['collect']]:
             await self.data_svc.update('core_chain', key='id', value=link['id'], data=dict(collect=datetime.now()))
-            payload = await self.data_svc.explode_payloads(criteria=dict(ability=link['ability']))
-            instructions.append(json.dumps(dict(id=link['id'], sleep=link['jitter'],
-                                                command=link['command'], cleanup=link['cleanup'],
-                                                payload=payload[0]['payload'] if payload else '')))
+            payload = await self._gather_payload(link['ability'])
+            instructions.append(json.dumps(dict(id=link['id'],
+                                                sleep=link['jitter'],
+                                                command=link['command'],
+                                                cleanup=link['cleanup'],
+                                                payload=payload)))
         return json.dumps(instructions)
 
     async def post_results(self, paw, link_id, output, status):
-        self.log.debug('[AGENT] posting results (%s)' % paw)
+        self.log.debug('%s posting results' % paw)
         finished = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         await self.data_svc.create_result(result=dict(link_id=link_id, output=output))
-        await self.data_svc.update('core_chain', key='id', value=link_id, data=dict(status=int(status), finish=finished))
+        await self.data_svc.update('core_chain', key='id', value=link_id,
+                                   data=dict(status=int(status), finish=finished))
         return json.dumps(dict(status=True))
+
+    """ PRIVATE """
+
+    async def _gather_payload(self, ability_id):
+        payload = await self.data_svc.explode_payloads(criteria=dict(ability=ability_id))
+        return payload[0]['payload'] if payload else ''
--- gocat/cleanup/cleanup.go
+++ gocat/cleanup/cleanup.go
@@ -2,25 +2,44 @@ package cleanup
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"../util"
 	"../execute"
 )
 
 var cleanups []string
+var payloads []string
 
 // Apply stores cleanup actions
 func Apply(command map[string]interface{}) {
 	if command["cleanup"] != nil && len(command["cleanup"].(string)) > 1 {
 		cleanups = append(cleanups, command["cleanup"].(string))
 	}
+	payload := command["payload"].(string)
+	if len(payload) > 0 && !alreadyHave(payload) {
+		payloads = append(payloads, payload)
+	}
 }
 
 // Run executes all cleanup activities
-func Run(files string) {
+func Run() {
 	for i := range cleanups {
 		cmd := util.Decode(cleanups[len(cleanups)-1-i])
 		fmt.Println(fmt.Sprintf("[+] Cleanup: %s", cmd))
 		execute.Execute(string(cmd))
 	}
+	for _, value := range payloads {
+		fmt.Println(fmt.Sprintf("[*] Removing payload: %s", value))
+		os.Remove(filepath.Join(value))
+	}
 	cleanups = cleanups[:0]
+	payloads = payloads[:0]
+}
+
+func alreadyHave(payload string) bool {
+    for _, p := range payloads {
+        if p == payload { return true }
+    }
+    return false
 }
