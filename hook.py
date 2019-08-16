b35764edd413e708d79fb30bdfd31e6f7fe3498a
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-08-16 14:40:48 -0400

    ensuring agents can be managed modular (#41)
    * ensuring agents can be managed modular
    
    * ensuring agents can be managed modular

--- hook.py
+++ hook.py
@@ -7,6 +7,6 @@ address = None
 
 async def initialize(app, services):
     cat_api = SandApi(services=services)
-    app.router.add_route('POST', '/sand/beacon', cat_api.beacon)
+    app.router.add_route('POST', '/sand/instructions', cat_api.instructions)
     app.router.add_route('POST', '/sand/results', cat_api.results)
 
