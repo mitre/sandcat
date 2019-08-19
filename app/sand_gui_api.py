f0d763e8146cc19ca7a56588bc47fab7710ecc1c
David Hunt <49954156+privateducky@users.noreply.github.com>
2019-08-19 15:54:03 -0400

    adding a gui to the sandcat plugin (#43)
    Former-commit-id: b21f662d65db9cd86bd7462a07d9461d3d20c762

---
+++ app/sand_gui_api.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+from aiohttp import web
+from aiohttp_jinja2 import template
+
+
+class SandGuiApi:
+
+    def __init__(self, services):
+        self.auth_svc = services.get('auth_svc')
+        self.utility_svc = services.get('utility_svc')
+
+    @template('sandcat.html')
+    async def splash(self, request):
+        await self.auth_svc.check_permissions(request)
+        return dict(site_status='up' if os.path.isfile('plugins/sandcat/static/malicious/index.html') else 'down')
+
+    @staticmethod
+    async def malicious(request):
+        return web.HTTPFound('/malicious/index.html')
+
+    async def clone_new_site(self, request):
+        await self.auth_svc.check_permissions(request)
+        url = request.rel_url.query['url']
+        location = 'plugins/sandcat/static/malicious/'
+        shutil.rmtree(location, ignore_errors=True)
+        os.system(f"""wget -E -H -k -K -p -q -nH --cut-dirs=1 %s --directory %s --no-check-certificate""" % (url, location))
+        self.utility_svc.prepend_to_file('%s/index.html' % location, '<script src="/sandcat/js/malicious.js"></script>')
+        self.utility_svc.prepend_to_file('%s/index.html' % location, '<meta http-equiv="Expires" content="0">')
+        self.utility_svc.prepend_to_file('%s/index.html' % location, '<meta http-equiv="Pragma" content="no-cache">')
+        self.utility_svc.prepend_to_file('%s/index.html' % location, '<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">')
+        return web.Response()
