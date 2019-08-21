e7eac49bc8288dd9194284bc0a0a1676bc1dd57d
hf-mitre <31480194+hf-mitre@users.noreply.github.com>
2019-08-20 20:28:19 -0400

    compatability with core after service refactor (#47)
    Former-commit-id: 9c751f6818158a482cc3abca64f1de7d7cf5b920

--- app/sand_api.py
+++ app/sand_api.py
@@ -7,20 +7,19 @@ from aiohttp import web
 class SandApi:
 
     def __init__(self, services):
-        self.utility_svc = services.get('utility_svc')
         self.file_svc = services.get('file_svc')
         self.agent_svc = services.get('agent_svc')
 
     async def instructions(self, request):
         paw = request.headers.get('X-PAW')
-        data = json.loads(self.utility_svc.decode_bytes(await request.read()))
+        data = json.loads(self.agent_svc.decode_bytes(await request.read()))
         data['server'] = '%s://%s' % (request.scheme, request.host)
         await self.agent_svc.handle_heartbeat(paw, **data)
         instructions = await self.agent_svc.get_instructions(paw)
-        return web.Response(text=self.utility_svc.encode_string(instructions))
+        return web.Response(text=self.agent_svc.encode_string(instructions))
 
     async def results(self, request):
-        data = json.loads(self.utility_svc.decode_bytes(await request.read()))
+        data = json.loads(self.agent_svc.decode_bytes(await request.read()))
         data['time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         status = await self.agent_svc.save_results(data['link_id'], data['output'], data['status'])
-        return web.Response(text=self.utility_svc.encode_string(status))
+        return web.Response(text=self.agent_svc.encode_string(status))
