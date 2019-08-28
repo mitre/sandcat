ce435782bd3b0a04beed9b3a96c7cc07062b693a
Alexander Manners <43142871+khyberspache@users.noreply.github.com>
2019-08-28 15:39:22 -0400

    multi platform executor handling implemented (#63)
    * multi platform executor handling implemented
    
    * fixing broken function
    
    * removing unecessary variable
    
    * moved executor selector code to execute package
    
    * moved variable to local scope in main and fixed a duplicate issue
    
    * Recompiled binaries for the new platforms

--- gocat/execute/execute.go
+++ gocat/execute/execute.go
@@ -1,15 +1,20 @@
 package execute
 
 import (
+	"fmt"
 	"os/exec"
+	"strings"
 )
+
+// ExecutorFlags type to import a list of executors
+type ExecutorFlags []string
 
 // Execute runs a shell command
 func Execute(command string, executor string) ([]byte, error) {
 	if executor == "psh" {
 		return exec.Command("powershell.exe", "-ExecutionPolicy", "Bypass", "-C", command).CombinedOutput()
 	} else if executor == "cmd" {
-		return exec.Command(command).CombinedOutput()
+		return exec.Command("cmd", "/C", command).CombinedOutput()
 	} else if executor == "pwsh" {
 		return exec.Command("pwsh", "-c", command).CombinedOutput()
 	}
@@ -20,7 +25,19 @@ func Execute(command string, executor string) ([]byte, error) {
 func DetermineExecutor(platform string) string {
 	if platform == "windows" {
 		return "psh"
-	} else {
-		return "sh"
 	}
+	return "sh"
+}
+
+// String get string format of input
+func (i *ExecutorFlags) String() string {
+	return fmt.Sprint((*i))
+}
+
+// Set value of the executor list
+func (i *ExecutorFlags) Set(value string) error {
+	for _, exec := range strings.Split(value, ",") {
+		*i = append(*i, exec)
+	}
+	return nil
 }
