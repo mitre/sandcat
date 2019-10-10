f4bba63840aa2732ca699f60effb74a4e04f2767
david <49954156+privateducky@users.noreply.github.com>
2019-10-10 07:26:25 -0400

    fixing a bug around how master p2p servers are found (#104)

--- gocat/util/utility.go
+++ gocat/util/utility.go
@@ -11,8 +11,6 @@ import (
 	"path/filepath"
 	"io"
 	"net/http"
-	"bufio"
-	"fmt"
 )
 
 // Encode base64 encodes bytes
@@ -90,22 +88,6 @@ func CheckPayloadsAvailable(payloads []string) []string {
 func StopProcess(pid int) {
 	proc, _ := os.FindProcess(pid)
 	_ = proc.Kill()
-}
-
-//ReadFile returns a list of lines for a given file
-func ReadFile(filepath string) []string {
-	file, err := os.Open(filepath)
-	if err != nil {
-		fmt.Println(err)
-	}
-    defer file.Close()
-
-	var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines
 }
 
 func removeWhiteSpace(input string) string {
