4877bee00f592d7cefc86c2b56008a7d5ec1d374
Henry Foster <hfoster@mitre.org>
2019-10-11 11:35:34 -0400

    updates after file_svc changes

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -13,20 +13,19 @@ class SandService:
         name, platform = headers.get('file'), headers.get('platform')
         if which('go') is not None:
             plugin, file_path = await self.file_svc.find_file_path(name)
-            await self._change_file_hash(file_path)
+
+            ldflags = ['-X main.key=%s' % (self._generate_key(),)]
+            for param in ('defaultServer', 'defaultGroup', 'defaultSleep'):
+                if param in headers:
+                    ldflags.append('-X main.%s=%s' % (param, headers[param]))
+
             output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
             self.file_svc.log.debug('Dynamically compiling %s' % name)
-            await self.file_svc.compile_go(platform, output, file_path)
+            await self.file_svc.compile_go(platform, output, file_path, ldflags=ldflags)
         return '%s-%s' % (name, platform)
 
     """ PRIVATE """
 
     @staticmethod
-    async def _change_file_hash(file_path, size=30):
-        key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
-        lines = open(file_path, 'r').readlines()
-        lines[-1] = 'var key = "%s"' % key
-        out = open(file_path, 'w')
-        out.writelines(lines)
-        out.close()
-        return key
+    async def _generate_key(size=30):
+        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
