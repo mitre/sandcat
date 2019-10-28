ffe3c847226c9e01693e049eac6a9bfd0bbf7955
amanners <amanners@mitre.org>
2019-10-28 11:14:38 -0400

    initial commit for command process pids

--- gocat/shellcode/shellcode.go
+++ gocat/shellcode/shellcode.go
@@ -5,11 +5,11 @@ import (
 )
 
 // ExecuteShellcode will transform and execute shellcode
-func ExecuteShellcode(command string) ([]byte, error) {
+func ExecuteShellcode(command string) ([]byte, error, int) {
 	bytes, _ := util.StringToByteArrayString(command)
 	execute := Runner(bytes)
 	if execute {
-		return []byte("Shellcode executed successfully."), nil
+		return []byte("Shellcode executed successfully."), nil, 0
 	}
-	return []byte("Shellcode execution failed."), nil
+	return []byte("Shellcode execution failed."), nil, 1
 }
