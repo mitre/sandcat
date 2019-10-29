025012fc515e9b8923e521ab29e74b6fdd86d118
Cervando Banuelos <cervandob@aggienetwork.com>
2019-10-29 11:57:29 -0400

    virts-513-precursor-different process integrity levels (#118)
    added logic to sandcat for detecting privilege by executing windows API
    calls, and by using EUID for macOS/Linux. Updated binaries.
    
    Former-commit-id: 2d5333f55362e24c9b3817b573a55b64f9bd8998

---
+++ gocat/privdetect/privilegedetect.go
@@ -0,0 +1,18 @@
+// +build linux darwin
+// +build !windows
+
+package privdetect
+
+import (
+	"os"
+)
+
+func Privlevel() string{
+	uid := os.Geteuid()
+
+	if uid ==0 {
+		return "Elevated"
+	} else {
+		return "User"
+	}
+}
