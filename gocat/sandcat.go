1e5af3f04464ece5fc3c2e1be02a7a48f94cdd35
amanners <amanners@mitre.org>
2019-10-30 10:05:37 -0400

    moving core code to package

--- gocat/sandcat.go
+++ gocat/sandcat.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"./execute"
 	"flag"
 
 	"./core"
-)
-
-/*
-These default  values can be overridden during linking - server, group, and sleep can also be overridden
-with command-line arguments at runtime.
-*/
-var (
-	key = "JWHQZM9Z4HQOYICDHW4OCJAXPPNHBA"
-	defaultServer = "http://localhost:8888"
-	defaultGroup = "my_group"
-	defaultSleep = "60"
+	"./execute"
 )
 
 func main() {
 	var executors execute.ExecutorFlags
-	server := flag.String("server", defaultServer, "The FQDN of the server")
-	group := flag.String("group", defaultGroup, "Attach a group to this agent")
-	sleep := flag.String("sleep", defaultSleep, "Initial sleep value for sandcat (integer in seconds)")
+	server := flag.String("server", core.DefaultServer, "The FQDN of the server")
+	group := flag.String("group", core.DefaultGroup, "Attach a group to this agent")
+	sleep := flag.String("sleep", core.DefaultSleep, "Initial sleep value for sandcat (integer in seconds)")
 	verbose := flag.Bool("v", false, "Enable verbose output")
 	flag.Var(&executors, "executors", "Comma separated list of executors (first listed is primary)")
 	flag.Parse()
