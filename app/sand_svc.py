07196ffaf3c511480af41f997aad3cc8f51e5956
amanners <amanners@mitre.org>
2019-10-31 12:33:13 -0400

    compiling works for shared libraries

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -11,19 +11,21 @@ class SandService:
         self.file_svc = file_svc
 
     async def dynamically_compile(self, headers):
-        name, platform, shared = headers.get('file'), headers.get('platform'), headers.get('shared')
-
+        name, platform = headers.get('file'), headers.get('platform')
         if which('go') is not None:
-            plugin, file_path = await self.file_svc.find_file_path(name)
-            path = os.path.dirname(os.path.abspath(file_path))
-            ldflags = ['-s', '-w', '-X _%s/core.Key=%s' % (path, self._generate_key(),)]
-            for param in ('DefaultServer', 'DefaultGroup', 'DefaultSleep'):
-                if param in headers:
-                    ldflags.append('-X _%s/core.%s=%s' % (path, param, headers[param]))
-
-            output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
-            self.file_svc.log.debug('Dynamically compiling %s' % name)
-            await self.file_svc.compile_go(platform, output, file_path, ldflags=' '.join(ldflags))
+            if name == 'shared.go':
+                await self._compile_new_agent(platform, headers,
+                                              compile_target_name=name,
+                                              output_name=name)
+            else:
+                await self._compile_new_agent(platform, headers,
+                                              compile_target_name=name.split('.')[0] + '_' + platform + '.go',
+                                              output_name=name,
+                                              extension='-lib',
+                                              extflags='-extldflags "-Wl,--nxcompat"',
+                                              custom_params='GOARCH=amd64 CGO_ENABLED=1 CC=X86_64-w64-mingw32-gcc')
+        if name == 'shared.go':
+            return '%s-%s-lib' % (name, platform)
         return '%s-%s' % (name, platform)
 
     """ PRIVATE """
@@ -31,3 +33,16 @@ class SandService:
     @staticmethod
     def _generate_key(size=30):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+    async def _compile_new_agent(self, platform, headers, compile_target_name, output_name, extension='', extflags='', custom_params=''):
+        plugin, file_path = await self.file_svc.find_file_path(compile_target_name)
+        path = os.path.dirname(os.path.abspath(file_path))
+        ldflags = ['-s', '-w', '-X _%s/core.Key=%s' % (path, self._generate_key(),)]
+        for param in ('DefaultServer', 'DefaultGroup', 'DefaultSleep'):
+            if param in headers:
+                ldflags.append('-X _%s/core.%s=%s' % (path, param, headers[param]))
+        output = 'plugins/%s/payloads/%s-%s%s' % (plugin, output_name, platform, extension)
+        ldflags.append(extflags)
+        self.file_svc.log.debug('Dynamically compiling %s' % compile_target_name)
+        await self.file_svc.compile_go(platform, output, file_path,
+                                       ldflags=' '.join(ldflags), custom_params=custom_params)
