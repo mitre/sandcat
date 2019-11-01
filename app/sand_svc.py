24e384f4ecc5b8139ba876593c65aa7dddc4e057
amanners <amanners@mitre.org>
2019-10-31 20:51:06 -0400

    compiling now works except for ASLR

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -18,8 +18,9 @@ class SandService:
                                               compile_target_name=name.split('.')[0] + '_' + platform + '.go',
                                               output_name=name,
                                               extension='-lib',
+                                              buildmode='--buildmode=c-shared',
                                               extflags='-extldflags "-Wl,--nxcompat"',
-                                              custom_params='GOARCH=amd64 CGO_ENABLED=1 CC=X86_64-w64-mingw32-gcc')
+                                              cflags='GOARCH=amd64 CGO_ENABLED=1 CC=X86_64-w64-mingw32-gcc')
             else:
                 await self._compile_new_agent(platform, headers,
                                               compile_target_name=name,
@@ -34,15 +35,16 @@ class SandService:
     def _generate_key(size=30):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
-    async def _compile_new_agent(self, platform, headers, compile_target_name, output_name, extension='', extflags='', custom_params=''):
+    async def _compile_new_agent(self, platform, headers, compile_target_name, output_name, buildmode='', extension='',
+                                 extflags='', cflags=''):
         plugin, file_path = await self.file_svc.find_file_path(compile_target_name)
         path = os.path.dirname(os.path.abspath(file_path))
-        ldflags = ['-s', '-w', '-X _%s/core.Key=%s' % (path, self._generate_key(),)]
-        for param in ('DefaultServer', 'DefaultGroup', 'DefaultSleep'):
+        ldflags = ['-s', '-w', '-X main.key=%s' % (self._generate_key(),)]
+        for param in ('defaultServer', 'defaultGroup', 'defaultSleep'):
             if param in headers:
-                ldflags.append('-X _%s/core.%s=%s' % (path, param, headers[param]))
+                ldflags.append('-X main.%s=%s' % (param, headers[param]))
         output = 'plugins/%s/payloads/%s-%s%s' % (plugin, output_name, platform, extension)
         ldflags.append(extflags)
         self.file_svc.log.debug('Dynamically compiling %s' % compile_target_name)
-        await self.file_svc.compile_go(platform, output, file_path,
-                                       ldflags=' '.join(ldflags), custom_params=custom_params)
+        await self.file_svc.compile_go(platform, output, file_path, buildmode=buildmode,
+                                       ldflags=' '.join(ldflags), cflags=cflags)
