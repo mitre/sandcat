08c9f98f73f38507ad048b77e1d32f31a6d42510
amanners <amanners@mitre.org>
2019-10-31 22:24:22 -0400

    actually check if mingw compiler is present

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -14,15 +14,18 @@ class SandService:
         name, platform = headers.get('file'), headers.get('platform')
         if which('go') is not None:
             if name == 'shared.go':
-                await self._compile_new_agent(platform, headers,
-                                              compile_target_name=name.split('.')[0] + '_' + platform + '.go',
-                                              output_name=name,
-                                              extension='-lib',
-                                              buildmode='--buildmode=c-shared',
-                                              extflags='-extldflags "-Wl,--nxcompat -Wl,--dynamicbase -Wl,--high-entropy-va"',
-                                              cflags='GOARCH=amd64 CGO_ENABLED=1 CC=X86_64-w64-mingw32-gcc')
+                if platform == 'windows' and which('X86_64-w64-mingw32-gcc'):
+                    await self._compile_new_agent(platform=platform,
+                                                  headers=headers,
+                                                  compile_target_name=name.split('.')[0] + '_' + platform + '.go',
+                                                  output_name=name,
+                                                  extension='-lib',
+                                                  buildmode='--buildmode=c-shared',
+                                                  extldflags='-extldflags "-Wl,--nxcompat -Wl,--dynamicbase -Wl,--high-entropy-va"',
+                                                  cflags='GOARCH=amd64 CGO_ENABLED=1 CC=X86_64-w64-mingw32-gcc')
             else:
-                await self._compile_new_agent(platform, headers,
+                await self._compile_new_agent(platform=platform,
+                                              headers=headers,
                                               compile_target_name=name,
                                               output_name=name)
         if name == 'shared.go':
@@ -36,15 +39,14 @@ class SandService:
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
     async def _compile_new_agent(self, platform, headers, compile_target_name, output_name, buildmode='', extension='',
-                                 extflags='', cflags=''):
+                                 extldflags='', cflags=''):
         plugin, file_path = await self.file_svc.find_file_path(compile_target_name)
-        path = os.path.dirname(os.path.abspath(file_path))
         ldflags = ['-s', '-w', '-X main.key=%s' % (self._generate_key(),)]
         for param in ('defaultServer', 'defaultGroup', 'defaultSleep'):
             if param in headers:
                 ldflags.append('-X main.%s=%s' % (param, headers[param]))
         output = 'plugins/%s/payloads/%s-%s%s' % (plugin, output_name, platform, extension)
-        ldflags.append(extflags)
+        ldflags.append(extldflags)
         self.file_svc.log.debug('Dynamically compiling %s' % compile_target_name)
         await self.file_svc.compile_go(platform, output, file_path, buildmode=buildmode,
                                        ldflags=' '.join(ldflags), cflags=cflags)
