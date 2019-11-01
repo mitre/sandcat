62697965d5a271bb722345a9b594e963d1354674
amanners <amanners@mitre.org>
2019-10-31 20:51:06 -0400

    compiling now works except for ASLR

--- gocat/sandcat.go
+++ gocat/sandcat.go
@@ -16,9 +16,9 @@ var (
 
 func main() {
 	var executors execute.ExecutorFlags
-	server := flag.String("server", core.DefaultServer, "The FQDN of the server")
-	group := flag.String("group", core.DefaultGroup, "Attach a group to this agent")
-	sleep := flag.String("sleep", core.DefaultSleep, "Initial sleep value for sandcat (integer in seconds)")
+	server := flag.String("server", defaultServer, "The FQDN of the server")
+	group := flag.String("group", defaultGroup, "Attach a group to this agent")
+	sleep := flag.String("sleep", defaultSleep, "Initial sleep value for sandcat (integer in seconds)")
 	verbose := flag.Bool("v", false, "Enable verbose output")
 	flag.Var(&executors, "executors", "Comma separated list of executors (first listed is primary)")
 	flag.Parse()
--- gocat/shared/shared_windows.go
+++ gocat/shared/shared_windows.go
@@ -9,6 +9,13 @@ var (
 	defaultSleep = "60"
 )
 
+var (
+	key = "JWHQZM9Z4HQOYICDHW4OCJAXPPNHBA"
+	defaultServer = "http://localhost:8888"
+	defaultGroup = "my_group"
+	defaultSleep = "60"
+)
+
 //export VoidFunc
 func VoidFunc() {
 	core.Core(defaultServer, defaultGroup, defaultSleep, []string{"psh","cmd"}, false)
