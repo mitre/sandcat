381a5370bc5379134128e283c2b2fa18522e9bb3
Hunt, David <davidhunt@mitre.org>
2019-11-19 14:48:14 -0500

    test

--- app/c2_api.py
+++ app/c2_api.py
@@ -1,20 +1,28 @@
 import json
 from datetime import datetime
-
-from aiohttp import web
 from urllib.parse import urlparse
 
+from aiohttp import web
 
-class SandApi:
+from app.base.c2 import C2
 
-    def __init__(self, services):
-        self.file_svc = services.get('file_svc')
+
+class Api(C2):
+
+    def __init__(self, app, services):
         self.agent_svc = services.get('agent_svc')
+        app.router.add_route('POST', '/sand/ping', self._ping)
+        app.router.add_route('POST', '/sand/instructions', self._instructions)
+        app.router.add_route('POST', '/sand/results', self._results)
+        super().__init__(services)
+        self.log = self.add_c2('api', self)
 
-    async def ping(self, request):
+    """ PRIVATE """
+
+    async def _ping(self, request):
         return web.Response(text=self.agent_svc.encode_string('pong'))
 
-    async def instructions(self, request):
+    async def _instructions(self, request):
         data = json.loads(self.agent_svc.decode_bytes(await request.read()))
         url = urlparse(data['server'])
         port = '443' if url.scheme == 'https' else 80
@@ -24,7 +32,7 @@ class SandApi:
         response = dict(sleep=await agent.calculate_sleep(), instructions=instructions)
         return web.Response(text=self.agent_svc.encode_string(json.dumps(response)))
 
-    async def results(self, request):
+    async def _results(self, request):
         data = json.loads(self.agent_svc.decode_bytes(await request.read()))
         data['time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         status = await self.agent_svc.save_results(data['id'], data['output'], data['status'], data['pid'])
--- hook.py
+++ hook.py
@@ -1,4 +1,4 @@
-from plugins.sandcat.app.sand_api import SandApi
+from plugins.sandcat.app.c2_api import Api
 from plugins.sandcat.app.sand_gui_api import SandGuiApi
 from plugins.sandcat.app.sand_svc import SandService
 
@@ -10,13 +10,7 @@ address = '/plugin/sandcat/gui'
 async def initialize(app, services):
     file_svc = services.get('file_svc')
     await file_svc.add_special_payload('sandcat.go', SandService(file_svc).dynamically_compile)
-
-    cat_api = SandApi(services=services)
     cat_gui_api = SandGuiApi(services=services)
     app.router.add_static('/sandcat', 'plugins/sandcat/static/', append_version=True)
-    # cat
-    app.router.add_route('POST', '/sand/ping', cat_api.ping)
-    app.router.add_route('POST', '/sand/instructions', cat_api.instructions)
-    app.router.add_route('POST', '/sand/results', cat_api.results)
-    # gui
     app.router.add_route('GET', '/plugin/sandcat/gui', cat_gui_api.splash)
+    Api(app, services)
