7f8215fb029f6e4744ac5b26a68fe5dd72197360
brianedmonds90 <bedmonds@mitre.org>
2019-11-20 14:34:32 -0500

    updating server-side with the new style

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -1,8 +1,8 @@
 import random
 import string
 
-from app.utility.base_service import BaseService
 from shutil import which
+from app.utility.base_service import BaseService
 
 
 class SandService(BaseService):
@@ -10,19 +10,20 @@ class SandService(BaseService):
     def __init__(self, services):
         self.file_svc = services.get('file_svc')
         self.data_svc = services.get('data_svc')
-        self.c2_svc = services.get('c2_svc')
 
     async def dynamically_compile(self, headers):
         name, platform = headers.get('file'), headers.get('platform')
         if which('go') is not None:
             plugin, file_path = await self.file_svc.find_file_path(name)
+
             ldflags = ['-s', '-w', '-X main.key=%s' % (self._generate_key(),)]
             for param in ('defaultServer', 'defaultGroup', 'defaultSleep', 'defaultC2', 'c2'):
                 if param in headers:
                     if param == 'c2':
-                        ldflags.append('-X main.%s=%s' % (await self._get_c2_requirement(headers[param])))
+                        ldflags.append('-X main.%s=%s' % (await self._get_c2_config(headers[param])))
                     else:
                         ldflags.append('-X main.%s=%s' % (param, headers[param]))
+
             output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
             self.file_svc.log.debug('Dynamically compiling %s' % name)
             await self.file_svc.compile_go(platform, output, file_path, ldflags=' '.join(ldflags))
@@ -34,13 +35,8 @@ class SandService(BaseService):
     def _generate_key(size=30):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
 
-    async def _get_c2_requirement(self, type):
-        var = 'githubToken'
-        if type.lower() == 'gist':
-            c2 = await self.data_svc.locate('c2', dict(enabled=True, c2_type='active'))
-            if len(c2):
-                c2_module = await self.load_module(module_type=c2[0].name, module_info=dict(module=c2[0].module,
-                                                                                            config=c2[0].config,
-                                                                                            c2_type=c2[0].c2_type))
-                return var, c2_module.get_key()
-        return var, ''
+    async def _get_c2_config(self, c2_type):
+        c2 = await self.data_svc.locate('c2', dict(name=c2_type))
+        if len(c2):
+            return c2[0].get_config()
+        return '', ''
--- hook.py
+++ hook.py
@@ -1,7 +1,3 @@
-import platform
-import subprocess
-
-from plugins.sandcat.app.sand_api import SandApi
 from plugins.sandcat.app.sand_gui_api import SandGuiApi
 from plugins.sandcat.app.sand_svc import SandService
 
@@ -14,19 +10,8 @@ async def initialize(app, services):
     file_svc = services.get('file_svc')
     await file_svc.add_special_payload('sandcat.go', SandService(services).dynamically_compile)
 
-    cat_api = SandApi(services=services)
     cat_gui_api = SandGuiApi(services=services)
     app.router.add_static('/sandcat', 'plugins/sandcat/static/', append_version=True)
-    # cat
-    app.router.add_route('POST', '/sand/ping', cat_api.ping)
-    app.router.add_route('POST', '/sand/instructions', cat_api.instructions)
-    app.router.add_route('POST', '/sand/results', cat_api.results)
+
     # gui
     app.router.add_route('GET', '/plugin/sandcat/gui', cat_gui_api.splash)
-    await _start_pet(group='pet')
-
-
-async def _start_pet(group):
-    subprocess.Popen(['./plugins/sandcat/payloads/sandcat.go-%s' % platform.system().lower(), '-group', group, '-delay', '3'])
-
-
