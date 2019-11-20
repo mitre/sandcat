d8d518383e60ac0268eb673c5c13ffb2f6b45371
brianedmonds90 <bedmonds@mitre.org>
2019-11-20 14:20:41 -0500

    only server-side changes

--- gocat/contact/contact.go
+++ gocat/contact/contact.go
@@ -1,21 +1,14 @@
-package contact
-
-const (
-	ok = 200
-	created = 201
-)
+package contact 
 
 //Contact defines required functions for communicating with the server
 type Contact interface {
 	Ping(server string) bool
 	GetInstructions(profile map[string]interface{}) map[string]interface{}
-	DropPayloads(payload string, server string, uniqueId string) []string
+	DropPayloads(payload string, server string) []string
 	RunInstruction(command map[string]interface{}, profile map[string]interface{}, payloads []string)
-	C2RequirementsMet(criteria interface{}) bool
 }
 
 //CommunicationChannels contains the contact implementations
 var CommunicationChannels = map[string]Contact{
 	"API": API{},
-	"GIST": GIST{},
 }
