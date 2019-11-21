397135f20b9277a1e50b8365024c05ff01d585ef
david <49954156+privateducky@users.noreply.github.com>
2019-11-21 11:25:44 -0500

    adding enable option to hook file (#152)
    Former-commit-id: d7f4087d3fe54134f029dd46e8cbc1df08e07716

--- app/sand_gui_api.py
+++ app/sand_gui_api.py
@@ -11,5 +11,5 @@ class SandGuiApi:
     @template('sandcat.html')
     async def splash(self, request):
         await self.auth_svc.check_permissions(request)
-        plugins = [dict(name=getattr(p, 'name'), address=getattr(p, 'address')) for p in self.app_svc.get_plugins()]
+        plugins = [p for p in await self.data_svc.locate('plugins', match=dict(enabled=True))]
         return dict(plugins=plugins)
