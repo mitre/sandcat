9491e0582744c19153249f20f3bf1d8826de4ec0
amanners <amanners@mitre.org>
2019-11-22 16:54:54 -0500

    single quotes

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -34,20 +34,20 @@ class SandService(BaseService):
     async def install_gocat_extensions(self):
         if which('go') is not None:
             if self._check_gist_go_dependencies():
-                self._copy_file_to_sandcat(file="gist.go", pkg="contact")
+                self._copy_file_to_sandcat(file='gist.go', pkg='contact')
 
     """ PRIVATE """
 
     @staticmethod
     def _check_gist_go_dependencies():
-        go_path = os.path.join(os.environ['GOPATH'], "src")
-        return os.path.exists(os.path.join(go_path, "github.com/google/go-github/github")) and \
-            os.path.exists(os.path.join(go_path, "golang.org/x/oauth2"))
+        go_path = os.path.join(os.environ['GOPATH'], 'src')
+        return os.path.exists(os.path.join(go_path, 'github.com/google/go-github/github')) and \
+            os.path.exists(os.path.join(go_path, 'golang.org/x/oauth2'))
 
     @staticmethod
     def _copy_file_to_sandcat(file, pkg):
-        base = os.path.abspath(os.path.join("plugins", "sandcat"))
-        copyfile(os.path.join(base, "gocat-extensions", pkg, file), os.path.join(base, "gocat", pkg, file))
+        base = os.path.abspath(os.path.join('plugins', 'sandcat'))
+        copyfile(os.path.join(base, 'gocat-extensions', pkg, file), os.path.join(base, 'gocat', pkg, file))
 
     @staticmethod
     def _generate_key(size=30):
