f0174187a7adba2fc57a38600c9ff6b4c26c652e
Brian Edmonds <bedmonds@mitre.org>
2019-11-22 10:47:49 -0500

    Gist c2 serverside (#150)
    * dynamic compilation changes to support gist
    
    * gist.go upload from Manners branch
    
    * getting files from Manners branch
    
    * reseting to master for execute.go
    
    * resetting to master
    
    * only server-side changes
    
    * removing gist.go
    
    * fixing nav bar colors (#151)
    
    * adding enable option to hook file (#152)
    
    * changing initialize to enable in hook (#153)
    
    * removing enable flag. not needed (#154)
    
    * dynamic compilation changes to support gist
    
    * gist.go upload from Manners branch
    
    * getting files from Manners branch
    
    * reseting to master for execute.go
    
    * resetting to master
    
    * only server-side changes
    
    * removing gist.go
    
    
    Former-commit-id: 45cd4cc5aadce7b190c2e0a8d822609bef5a5e11

--- app/sand_svc.py
+++ app/sand_svc.py
@@ -2,12 +2,14 @@ import random
 import string
 
 from shutil import which
+from app.utility.base_service import BaseService
 
 
-class SandService:
+class SandService(BaseService):
 
-    def __init__(self, file_svc):
-        self.file_svc = file_svc
+    def __init__(self, services):
+        self.file_svc = services.get('file_svc')
+        self.data_svc = services.get('data_svc')
 
     async def dynamically_compile(self, headers):
         name, platform = headers.get('file'), headers.get('platform')
@@ -15,9 +17,12 @@ class SandService:
             plugin, file_path = await self.file_svc.find_file_path(name)
 
             ldflags = ['-s', '-w', '-X main.key=%s' % (self._generate_key(),)]
-            for param in ('defaultServer', 'defaultGroup', 'defaultSleep'):
+            for param in ('defaultServer', 'defaultGroup', 'defaultSleep', 'defaultC2', 'c2'):
                 if param in headers:
-                    ldflags.append('-X main.%s=%s' % (param, headers[param]))
+                    if param == 'c2':
+                        ldflags.append('-X main.%s=%s' % (await self._get_c2_config(headers[param])))
+                    else:
+                        ldflags.append('-X main.%s=%s' % (param, headers[param]))
 
             output = 'plugins/%s/payloads/%s-%s' % (plugin, name, platform)
             self.file_svc.log.debug('Dynamically compiling %s' % name)
@@ -29,3 +34,9 @@ class SandService:
     @staticmethod
     def _generate_key(size=30):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+    async def _get_c2_config(self, c2_type):
+        c2 = await self.data_svc.locate('c2', dict(name=c2_type))
+        if len(c2):
+            return c2[0].get_config()
+        return '', ''
