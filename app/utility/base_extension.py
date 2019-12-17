666a52c6325790b515e4bb41b3cc30b251abc73e
brianedmonds90 <bedmonds@mitre.org>
2019-12-17 11:24:55 -0500

    new way of checking for go dependencies

--- app/utility/base_extension.py
+++ app/utility/base_extension.py
@@ -9,22 +9,10 @@ class Extension(ABC):
     def __init__(self, file, package):
         self.file = file
         self.package = package
-        self.go_src_path = self._check_go_src_path()
         self.dependencies = []
 
     def check_go_dependencies(self):
-        if self.go_src_path:
-            for d in self.dependencies:
-                if not os.path.exists(os.path.join(self.go_src_path, d)):
-                    return False
-            return True
-        return False
-
-    """ PRIVATE """
-
-    @staticmethod
-    def _check_go_src_path():
-        try:
-            return os.path.join(os.environ['GOPATH'], 'src')
-        except Exception:
-            return None
+        for d in self.dependencies:
+            if os.system('go list "{}"'.format(d)) != 0:
+                return False
+        return True
