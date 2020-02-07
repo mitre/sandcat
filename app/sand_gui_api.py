609b4ad1d90116eec282dc9c6d923367df4189e0
Hunt, David <davidhunt@mitre.org>
2020-02-07 07:53:01 -0700

    using new app config that is a singular object part of base world

--- app/sand_gui_api.py
+++ app/sand_gui_api.py
@@ -11,4 +11,5 @@ class SandGuiApi:
     @template('sandcat.html')
     async def splash(self, request):
         await self.auth_svc.check_permissions(request)
-        return dict()
+        delivery_cmds = await self.data_svc.locate('abilities', dict(ability_id='2f34977d-9558-4c12-abad-349716777c6b'))
+        return dict(delivery_cmds=delivery_cmds)
