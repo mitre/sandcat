c5e427df267b5830f74528d491eef80430ef5e13
david <49954156+privateducky@users.noreply.github.com>
2020-02-16 14:01:02 -0500

    splitting out delivery command to red and blue while determining whether abilities should use params (#247)

--- app/sand_gui_api.py
+++ app/sand_gui_api.py
@@ -1,9 +1,10 @@
 from aiohttp_jinja2 import template
 
 from app.service.auth_svc import check_authorization
+from app.utility.base_world import BaseWorld
 
 
-class SandGuiApi:
+class SandGuiApi(BaseWorld):
 
     def __init__(self, services):
         self.auth_svc = services.get('auth_svc')
@@ -13,5 +14,10 @@ class SandGuiApi:
     @check_authorization
     @template('sandcat.html')
     async def splash(self, request):
-        delivery_cmds = await self.data_svc.locate('abilities', dict(ability_id='2f34977d-9558-4c12-abad-349716777c6b'))
+        red_ability = '2f34977d-9558-4c12-abad-349716777c6b'
+        blue_ability = 'b02f97e2-6be3-4583-9080-7c3b1e7b573e'
+        access = dict(access=tuple(await self.auth_svc.get_permissions(request)))
+        delivery_cmds = await self.data_svc.locate('abilities', dict(
+            ability_id=blue_ability if self.Access.BLUE in access['access'] else red_ability)
+        )
         return dict(delivery_cmds=delivery_cmds)
