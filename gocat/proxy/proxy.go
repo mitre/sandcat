c286711436bf903fbad7d561f97d42d86063b276
Daniel Kim <kimd@mitre.org>
2020-03-03 09:29:44 -0500

    Fixed tab spacing

--- gocat/proxy/proxy.go
+++ gocat/proxy/proxy.go
@@ -23,11 +23,11 @@ type P2pReceiver interface {
 
 // Defines message structure for p2p
 type P2pMessage struct {
-    RequestingAgentPaw string // Paw of agent sending the original request.
-    SourceAddress string // return address for responses (e.g. IP + port, pipe path)
-    MessageType int
-    Payload []byte
-    Populated bool
+	RequestingAgentPaw string // Paw of agent sending the original request.
+	SourceAddress string // return address for responses (e.g. IP + port, pipe path)
+	MessageType int
+	Payload []byte
+	Populated bool
 }
 
 // P2pReceiverChannels contains the P2pReceiver implementations
@@ -40,27 +40,27 @@ var P2pClientChannels = map[string]contact.Contact{}
 
 // Build p2p message and return the bytes of its JSON marshal.
 func BuildP2pMsgBytes(paw string, messageType int, payload []byte, srcAddr string) []byte {
-    p2pMsg := &P2pMessage{
-        RequestingAgentPaw: paw,
-        SourceAddress: srcAddr,
-        MessageType: messageType,
-        Payload: payload,
-        Populated: true,
-    }
-    p2pMsgData, _ := json.Marshal(p2pMsg)
-    return p2pMsgData
+	p2pMsg := &P2pMessage{
+		RequestingAgentPaw: paw,
+		SourceAddress: srcAddr,
+		MessageType: messageType,
+		Payload: payload,
+		Populated: true,
+	}
+	p2pMsgData, _ := json.Marshal(p2pMsg)
+	return p2pMsgData
 }
 
 // Convert bytes of JSON marshal into P2pMessage struct
 func BytesToP2pMsg(data []byte) P2pMessage {
-    var message P2pMessage
-    if err := json.Unmarshal(data, &message); err == nil {
-        return message
-    }
-    return P2pMessage{}
+	var message P2pMessage
+	if err := json.Unmarshal(data, &message); err == nil {
+		return message
+	}
+	return P2pMessage{}
 }
 
 // Check if message is empty.
 func MsgIsEmpty(msg P2pMessage) bool {
-    return !msg.Populated
+	return !msg.Populated
 }
