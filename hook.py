272b1ab0b745fef0e45f36090f3805e61061b396
Hunt, David <davidhunt@mitre.org>
2020-03-04 14:33:53 -0500

    adding ability command variation as an option and ensuring all agents are marked as initial access

--- hook.py
+++ hook.py
@@ -1,16 +1,12 @@
-from plugins.sandcat.app.sand_gui_api import SandGuiApi
 from plugins.sandcat.app.sand_svc import SandService
 
 name = 'Sandcat'
 description = 'A custom multi-platform RAT'
-address = '/plugin/sandcat/gui'
+address = None
 
 
 async def enable(services):
-    app = services.get('app_svc').application
     file_svc = services.get('file_svc')
     sand_svc = SandService(services)
     await file_svc.add_special_payload('sandcat.go', sand_svc.dynamically_compile_executable)
     await file_svc.add_special_payload('shared.go', sand_svc.dynamically_compile_library)
-    cat_gui_api = SandGuiApi(services=services)
-    app.router.add_route('GET', '/plugin/sandcat/gui', cat_gui_api.splash)
