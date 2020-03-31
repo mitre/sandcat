3f57238b6bb990cf02f0ae7798e472342743ee4d
Tran, Chris <ctran@mitre.org>
2020-03-31 16:42:51 -0400

    add conversion of string P2P flag to boolean for core call

--- gocat/shared/shared.go
+++ gocat/shared/shared.go
@@ -4,6 +4,8 @@ package main
 
 import "C"
 import (
+	"strconv"
+
 	"github.com/mitre/sandcat/gocat/core"
 )
 
@@ -11,15 +13,17 @@ var (
 	key       = "JWHQZM9Z4HQOYICDHW4OCJAXPPNHBA"
 	server    = "http://localhost:8888"
 	group     = "red"
-	listenP2P = false
+	listenP2P = "false"
 	c2Name    = "HTTP"
 	c2Key     = ""
 )
 
 //export VoidFunc
 func VoidFunc() {
+	parsedListenP2P, _ := strconv.ParseBool(listenP2P)
+
 	c2Config := map[string]string{"c2Name": c2Name, "c2Key": c2Key}
-	core.Core(server, group, 0, nil, c2Config, listenP2P, false)
+	core.Core(server, group, 0, nil, c2Config, parsedListenP2P, false)
 }
 
 func main() {}
