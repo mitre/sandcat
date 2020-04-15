a2bfe04733c688a380e9e795fb98cf7ef4ad6cfc
TheWover <thewover@protonmail.ch>
2020-04-15 14:29:50 -0500

    modified injection to use remote process with hardcoded PID

--- gocat/executors/donut/donut_windows.go
+++ gocat/executors/donut/donut_windows.go
@@ -26,10 +26,18 @@ func init() {
 func (d *Donut) Run(command string, timeout int) ([]byte, string, string) {
 	bytes, _ := ioutil.ReadFile("something.donut")
 
+	// Create the new host process, getting its output
+
+	//Pass the shellcode bytes and PID of the host process to the Runner
+
+
+
 	//gToByteArrayString(string(content))
 
 	//bytes, _ := util.StringToByteArrayString(command)
 	//task, pid := Runner(bytes)
+
+	//return status and also output of process
 
 	task, pid := Runner(bytes)
 	if task {
