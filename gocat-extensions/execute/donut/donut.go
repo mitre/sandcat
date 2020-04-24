f9e31389f49cc249dfe0ec89e1e574d82521280b
amanners <amanners@mitre.org>
2020-04-23 20:24:37 -0400

    here is the format of the PR

---
+++ gocat-extensions/execute/donut/donut.go
@@ -0,0 +1,68 @@
+// +build windows
+
+package donut
+
+
+import (
+	"fmt"
+	"io/ioutil"
+	"runtime"
+
+	"github.com/mitre/gocat/execute"
+)
+
+type Donut struct {
+	archName string
+}
+
+func init() {
+	runner := &Donut{
+		archName: "donut_"+runtime.GOARCH,
+	}
+	if runner.CheckIfAvailable() {
+		execute.Executors[runner.archName] = runner
+	}
+}
+
+const COMMANDLINE = "rundll32.exe"
+
+func (d *Donut) Run(command string, timeout int) ([]byte, string, string) {
+	bytes, _ := ioutil.ReadFile(command)
+
+	handle, pid, stdout, stderr := CreateSuspendedProcessWithIORedirect("rundll32.exe")
+
+	//Setup variables
+	stdoutBytes := make([]byte, 1)
+	stderrBytes := make([]byte, 1)
+	var eventCode uint32
+
+	// Run the shellcode and wait for it to complete
+	task, err := Runner(bytes, handle, stdout, &stdoutBytes, stderr, &stderrBytes, &eventCode)
+
+	if task {
+
+		// Assemble the final output
+
+		total := "Shellcode thread Exit Code: " + fmt.Sprint(eventCode) + "\n\n"
+
+		total += "STDOUT:\n"
+		total += string(stdoutBytes)
+		total += "\n\n"
+
+		total += "STDERR:\n"
+		total += string(stderrBytes)
+
+		return []byte(total), execute.SUCCESS_STATUS, fmt.Sprint(pid)
+	}
+
+	//Covers the cases where an error was received before the remote thread was created
+	return []byte(fmt.Sprintf("Shellcode execution failed. Error message: %s", fmt.Sprint(err))), execute.ERROR_STATUS, fmt.Sprint(pid)
+}
+
+func (d *Donut) String() string {
+	return d.archName
+}
+
+func (d *Donut) CheckIfAvailable() bool {
+	return IsAvailable()
+}
---
+++ gocat-extensions/execute/donut/donut_windows.go
@@ -0,0 +1,131 @@
+// +build windows
+
+package donut
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+// Runner runner
+func Runner(donut []byte, handle syscall.Handle, stdout syscall.Handle, stdoutBytes *[]byte, stderr syscall.Handle, stderrBytes *[]byte, eventCode *uint32) (bool, error) {
+
+	address, err := VirtualAllocEx(handle, 0, uintptr(len(donut)), MEM_COMMIT|MEM_RESERVE, syscall.PAGE_EXECUTE_READ)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+	var bytesWritten uintptr
+
+	_, err = WriteProcessMemory(handle, address, (uintptr)(unsafe.Pointer(&donut[0])), uintptr(len(donut)), &bytesWritten)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+	var threadHandle syscall.Handle
+
+	threadHandle, err = CreateRemoteThread(handle, nil, 0, address, 0, 0, 0)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+
+	*eventCode, err = WaitForSingleObject(threadHandle, 0xFFFFFFFF)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+	err = ReadFromPipes(stdout, stdoutBytes, stderr, stderrBytes)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+	//Close the thread handle
+	err = syscall.CloseHandle(threadHandle)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+
+	//Terminate the sacrificial process
+	err = TerminateProcess(handle, 0)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+
+	//close Process Handle
+	err = syscall.CloseHandle(handle)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+
+	//close stdout Handle
+	err = syscall.CloseHandle(stdout)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+
+	//close stderr Handle
+	err = syscall.CloseHandle(stderr)
+	if checkErrorMessage(err) {
+		return false, err
+	}
+
+	return true, err
+}
+
+func Cleanup(prochandle syscall.Handle, threadHandle syscall.Handle, stdout syscall.Handle, stderr syscall.Handle) (err error) {
+
+	//Close the thread handle
+	err = syscall.CloseHandle(threadHandle)
+	if checkErrorMessage(err) {
+		return err
+	}
+
+
+	//Terminate the sacrificial process
+	err = TerminateProcess(prochandle, 0)
+	if checkErrorMessage(err) {
+		return err
+	}
+
+
+	//close Process Handle
+	err = syscall.CloseHandle(prochandle)
+	if checkErrorMessage(err) {
+		return err
+	}
+
+
+	//close stdout Handle
+	err = syscall.CloseHandle(stdout)
+	if checkErrorMessage(err) {
+		return err
+	}
+
+
+	//close stderr Handle
+	err = syscall.CloseHandle(stderr)
+	if checkErrorMessage(err) {
+		return err
+	}
+
+	return err
+
+}
+
+// IsAvailable does a donut runner exist
+func IsAvailable() bool {
+
+	return true
+}
+
+func checkErrorMessage(err error) bool {
+	if err != nil && err.Error() != "The operation completed successfully." {
+		println(err.Error())
+		return true
+	}
+	return false
+}
