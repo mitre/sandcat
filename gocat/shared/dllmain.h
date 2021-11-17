9e298d8e868c0a6cd7db03685269c918a97f26ef
Daniel Kim <kimd@mitre.org>
2021-11-17 11:42:31 -0500

    Removing unnecessary files

--- gocat/shared/dllmain.h
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-void OnProcessAttach(HINSTANCE, DWORD, LPVOID);
-
-BOOL WINAPI DllMain(
-    HINSTANCE _hinstDLL,  // handle to DLL module
-    DWORD _fdwReason,     // reason for calling function
-    LPVOID _lpReserved    // reserved
-);
