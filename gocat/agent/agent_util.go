830629ad0dcac16506e3228e3808c175a38228a8
Daniel Kim <kimd@mitre.org>
2021-12-03 14:35:48 -0500

    Using full file path for agent executable loc for linux/darwin

--- gocat/agent/agent_util.go
+++ gocat/agent/agent_util.go
@@ -1,10 +1,13 @@
 package agent
 
 import (
+	"fmt"
 	"os"
 	"os/user"
 	"os/exec"
 	"time"
+
+	"github.com/mitre/gocat/output"
 )
 
 // Checks for a file
@@ -50,4 +53,14 @@ func getUsername() (string, error) {
 
 func getFormattedTimestamp(timestamp time.Time, dateFormat string) (string) {
     return timestamp.Format(dateFormat)
+}
+
+func getExecutablePath() (string) {
+	path, err := os.Executable()
+	if err != nil {
+		output.VerbosePrint(fmt.Sprintf("Error obtaining executable path: %s", err.Error()))
+		output.VerbosePrint("Obtaining path from command-line argument instead.")
+		return os.Args[0]
+	}
+	return path
 }
