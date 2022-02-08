4f3f4e20f6ba09d143b705a7a398118836a6127a
Daniel Kim <kimd@mitre.org>
2022-02-08 13:23:10 -0500

    Adding unit tests for disk payloads

---
+++ gocat/payload/disk_test.go
@@ -0,0 +1,96 @@
+package payload
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+const (
+	FILE_NAME = "dummyfile.txt"
+)
+
+var (
+	PAYLOAD_BYTES = []byte("sample payload data")
+)
+
+func TestWriteToDisk(t *testing.T) {
+	loc, err := WriteToDisk(FILE_NAME, PAYLOAD_BYTES)
+	if err != nil {
+		t.Errorf("Failed to write file to disk: %s", err.Error())
+	} else {
+		clearFile(t, FILE_NAME)
+	}
+	if loc != FILE_NAME {
+		t.Errorf("Got %s as returned file location; expected %s", loc, FILE_NAME)
+	}
+}
+
+func TestWriteToDiskAlreadyExisting(t *testing.T) {
+	err := touchFile(FILE_NAME)
+	if err != nil {
+		t.Errorf("Failed to touch file %s: %s",FILE_NAME, err.Error())
+		return
+	}
+	loc, err := WriteToDisk(FILE_NAME, PAYLOAD_BYTES)
+	if err != nil {
+		t.Errorf("Unexpected error for already existing payload: %s", err.Error())
+	}
+	if loc != FILE_NAME {
+		t.Errorf("Got %s as returned file location; expected %s", loc, FILE_NAME)
+	}
+	clearFile(t, FILE_NAME)
+}
+
+func TestCheckIfOnDisk(t *testing.T) {
+	err := touchFile(FILE_NAME)
+	if err != nil {
+		t.Errorf("Failed to touch file %s: %s",FILE_NAME, err.Error())
+		return
+	}
+	toCheck := []string{
+		"payload1",
+		"payload2",
+		FILE_NAME,
+		"payload3",
+	}
+	want := []string{
+		"payload1",
+		"payload2",
+		"payload3",
+	}
+	missing := CheckIfOnDisk(toCheck)
+	clearFile(t, FILE_NAME)
+	if !reflect.DeepEqual(missing, want) {
+		t.Errorf("Got %v, expected %v", missing, want)
+	}
+}
+
+func TestFileExists(t *testing.T) {
+	if FileExists(FILE_NAME) {
+		t.Errorf("File %s should not exist", FILE_NAME)
+	}
+	err := touchFile(FILE_NAME)
+	if err != nil {
+		t.Errorf("Failed to touch file %s: %s",FILE_NAME, err.Error())
+		return
+	}
+	if !FileExists(FILE_NAME) {
+		t.Errorf("File %s should exist", FILE_NAME)
+	}
+	clearFile(t, FILE_NAME)
+}
+
+func clearFile(t *testing.T, path string) {
+	if err := os.Remove(path); err != nil {
+		t.Errorf("Failed to clear file after test: %s", err.Error())
+	}
+}
+
+func touchFile(path string) error {
+	file, err := os.OpenFile(path, os.O_CREATE, 0700)
+	if err != nil {
+		return err
+	}
+	return file.Close()
+}
