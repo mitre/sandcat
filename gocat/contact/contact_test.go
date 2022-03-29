b1372f4fd410ce358531a4f112bc7c75470267d3
Govardhen Arunagiri <garunagiri@mitre.org>
2022-03-29 12:33:02 -0400

    added test for SupportsContinuous

---
+++ gocat/contact/contact_test.go
@@ -0,0 +1,36 @@
+package contact_test
+
+import (
+	"strconv"
+	"testing"
+
+	"github.com/mitre/gocat/contact"
+)
+
+var continuousContacts []string = make([]string, 0)
+
+func TestSupportsContinuous(t *testing.T) {
+	for contactName, contactImpl := range contact.CommunicationChannels {
+		var want bool
+		if contains(continuousContacts, contactName) {
+			want = true
+		} else {
+			want = false
+		}
+		result := contactImpl.SupportsContinuous()
+		if want != result {
+			t.Errorf("%s SupportsContinuous() should return %s, but returned %s", contactName, strconv.FormatBool(want), strconv.FormatBool(result))
+		}
+	}
+
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
