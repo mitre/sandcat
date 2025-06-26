7cbed56e0bdf1b3904025d14b3ac17f2053feffc
Daniel Kim <58484522+uruwhy@users.noreply.github.com>
2025-06-26 14:18:15 -0400

    fix whitespace

--- gocat-extensions/shared/shared.go
+++ gocat-extensions/shared/shared.go
@@ -6,30 +6,30 @@ import "C"
 
 import (
     "strconv"
-	"strings"
+    "strings"
 
-	"github.com/mitre/gocat/contact"
-	"github.com/mitre/gocat/core"
+    "github.com/mitre/gocat/contact"
+    "github.com/mitre/gocat/core"
 )
 
 var (
     key        = "JWHQZM9Z4HQOYICDHW4OCJAXPPNHBA"
-	server     = "http://localhost:8888"
-	paw        = ""
-	group      = "red"
-	c2Protocol = "HTTP"
-	c2Key      = ""
-	listenP2P  = "false" // need to set as string to allow ldflags -X build-time variable change on server-side.
+    server     = "http://localhost:8888"
+    paw        = ""
+    group      = "red"
+    c2Protocol = "HTTP"
+    c2Key      = ""
+    listenP2P  = "false" // need to set as string to allow ldflags -X build-time variable change on server-side.
     runOnInit  = "false" // need to set as string to allow ldflags -X build-time variable change on server-side.
-	httpProxyGateway = ""
+    httpProxyGateway = ""
     running    = false
 )
 
 func init() {
     parsedRunOnInit, err := strconv.ParseBool(runOnInit)
-	if err != nil {
-		parsedRunOnInit = false
-	}
+    if err != nil {
+        parsedRunOnInit = false
+    }
 
     if parsedRunOnInit {
         VoidFunc()
@@ -44,21 +44,21 @@ func VoidFunc() {
 
     running = true
     parsedListenP2P, err := strconv.ParseBool(listenP2P)
-	if err != nil {
-		parsedListenP2P = false
-	}
+    if err != nil {
+        parsedListenP2P = false
+    }
 
-	trimmedServer := strings.TrimRight(server, "/")
-	contactConfig := map[string]string{
-		"c2Name": c2Protocol,
-		"c2Key": c2Key,
-		"httpProxyGateway": httpProxyGateway,
-	}
-	tunnelConfig, err := contact.BuildTunnelConfig("", "", trimmedServer, "", "")
-	if err != nil {
-		return
-	}
-	core.Core(trimmedServer, tunnelConfig, group, 0, contactConfig, parsedListenP2P, false, paw, "")
+    trimmedServer := strings.TrimRight(server, "/")
+    contactConfig := map[string]string{
+        "c2Name": c2Protocol,
+        "c2Key": c2Key,
+        "httpProxyGateway": httpProxyGateway,
+    }
+    tunnelConfig, err := contact.BuildTunnelConfig("", "", trimmedServer, "", "")
+    if err != nil {
+        return
+    }
+    core.Core(trimmedServer, tunnelConfig, group, 0, contactConfig, parsedListenP2P, false, paw, "")
 }
 
 func main() {}
