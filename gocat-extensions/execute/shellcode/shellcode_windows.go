4ce684a9cbb57e283d75cc463460195c7e162cbb
uruwhy <58484522+uruwhy@users.noreply.github.com>
2025-08-20 03:57:42 +0000

    properly grab APIs for shellcode

--- gocat-extensions/execute/shellcode/shellcode_windows.go
+++ gocat-extensions/execute/shellcode/shellcode_windows.go
@@ -17,19 +17,19 @@ const (
 )
 
 var (
-	kernel32      *syscall.DLL
-	ntdll         *syscall.DLL
-	VirtualAlloc  *syscall.Proc
-	RtlCopyMemory *syscall.Proc
+	hKernel32      *syscall.DLL
+	hNtdll         *syscall.DLL
+	fpVirtualAlloc  *syscall.Proc
+	fpRtlCopyMemory *syscall.Proc
 )
 
 // Runner runner
 func Runner(shellcode []byte) (bool, string) {
-	address, _, err := VirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE)
+	address, _, err := fpVirtualAlloc.Call(0, uintptr(len(shellcode)), MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE)
 	if checkErrorMessage(err) {
 		return false, execute.ERROR_PID
 	}
-	_, _, err = RtlCopyMemory.Call(address, (uintptr)(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
+	_, _, err = fpRtlCopyMemory.Call(address, (uintptr)(unsafe.Pointer(&shellcode[0])), uintptr(len(shellcode)))
 	if checkErrorMessage(err) {
 		return false, execute.ERROR_PID
 	}
@@ -39,20 +39,27 @@ func Runner(shellcode []byte) (bool, string) {
 
 // IsAvailable does a shellcode runner exist
 func IsAvailable() bool {
-	if kernel32, kernel32Err := syscall.LoadDLL("kernel32.dll"); kernel32Err == nil {
-		if _, vAllocErr := kernel32.FindProc("VirtualAlloc"); vAllocErr != nil {
-			fmt.Printf("[-] VirtualAlloc error: %s", vAllocErr.Error())
+	var kernel32Err error
+	var ntdllErr error
+	var vAllocErr error
+	var rtlCopyMemErr error
+	if hKernel32, kernel32Err = syscall.LoadDLL("kernel32.dll"); kernel32Err == nil {
+		if fpVirtualAlloc, vAllocErr = hKernel32.FindProc("VirtualAlloc"); vAllocErr != nil {
+			fmt.Printf("[-] Failed to load VirtualAlloc API: %s", vAllocErr.Error())
 			return false
 		}
-		if ntdll, ntdllErr := syscall.LoadDLL("ntdll.dll"); ntdllErr == nil {
-			if _, rtlCopyMemErr := ntdll.FindProc("RtlCopyMemory"); rtlCopyMemErr == nil {
+		if hNtdll, ntdllErr = syscall.LoadDLL("ntdll.dll"); ntdllErr == nil {
+			if fpRtlCopyMemory, rtlCopyMemErr = hNtdll.FindProc("RtlCopyMemory"); rtlCopyMemErr == nil {
+				fmt.Printf("[+] Fetched required APIs for shellcode runner.")
 				return true
 			} else {
-				fmt.Printf("[-] RtlCopyMemory error: %s", rtlCopyMemErr.Error())
+				fmt.Printf("[-] Failed to load RtlCopyMemory API: %s", rtlCopyMemErr.Error())
 			}
+		} else {
+			fmt.Printf("[!] Failed to load NTDLL: %s", ntdllErr.Error())
 		}
 	} else {
-		fmt.Printf("[-] LoadDLL error: %s", kernel32Err.Error())
+		fmt.Printf("[-] Failed to load Kernel32: %s", kernel32Err.Error())
 	}
 	return false
 }
