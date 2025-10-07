accfe144c29d3f9bb0c27edfc6f2f29f6fa215ad
Daniel Matthews <58484522+uruwhy@users.noreply.github.com>
2025-10-07 13:33:41 -0400

    correctly log warning

--- hook.py
+++ hook.py
@@ -16,7 +16,7 @@ async def enable(services):
     await file_svc.add_special_payload('shared.go', sand_svc.dynamically_compile_library)
     # sudo apt install gcc-mingw-w64-x86-64
     if not which('x86_64-w64-mingw32-gcc'):
-        self.app.log.warn('x86_64-w64-mingw32-gcc dependency missing. Will not be able to compile sandcat as a Windows DLL.')
+        app.log.warn('x86_64-w64-mingw32-gcc dependency missing. Will not be able to compile sandcat as a Windows DLL.')
     cat_gui_api = SandGuiApi(services=services)
     app.router.add_static('/sandcat', 'plugins/sandcat/static', append_version=True)
     app.router.add_route('GET', '/plugin/sandcat/gui', cat_gui_api.splash)
